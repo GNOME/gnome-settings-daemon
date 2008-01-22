1198321137c88722ced4150eb0eef3515075af9f
Carlos Garnacho <carlosg@gnome.org>
2008-01-22 16:15:41 +0000

    Reworked, add a more appealing animation if there's a composite manager
    2008-01-22  Carlos Garnacho  <carlosg@gnome.org>
    
            * plugins/mouse/gsd-locate-pointer.c: Reworked, add a more appealing
            animation if there's a composite manager present, also use a similar
            animation for the non-composite case, so most of the code is shared.
            * plugins/mouse/gsd-timeline.[ch]: New files, object to control the
            "locate pointer" animation.
            * plugins/mouse/Makefile.am: Added these files to build.
    
    svn path=/trunk/; revision=57

--- plugins/mouse/gsd-locate-pointer.c
+++ plugins/mouse/gsd-locate-pointer.c
@@ -1,243 +1,293 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+/* gsd-locate-pointer.c
  *
- * Copyright © 2001 Jonathan Blandford <jrb@gnome.org>
+ * Copyright (C) 2008 Carlos Garnacho  <carlos@imendio.com>
  *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation, and that the name of Red Hat not be used in advertising or
- * publicity pertaining to distribution of the software without specific,
- * written prior permission.  Red Hat makes no representations about the
- * suitability of this software for any purpose.  It is provided "as is"
- * without express or implied warranty.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * Authors:  Jonathan Blandford
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #include <gtk/gtk.h>
-
+#include "gsd-timeline.h"
 #include "gsd-locate-pointer.h"
 
-#define LARGE_SIZE 101
-#define SMALL_SIZE 51
+#define ANIMATION_LENGTH 750
+#define WINDOW_SIZE 101
+#define N_CIRCLES 4
 
-typedef enum {
-  STAGE_ONE,
-  STAGE_TWO,
-  STAGE_THREE,
-  STAGE_FOUR,
-  STAGE_DONE
-} LocatePointerStage;
+/* All circles are supposed to be moving when progress
+ * reaches 0.5, and each of them are supposed to long
+ * for half of the progress, hence the need of 0.5 to
+ * get the circles interval, and the multiplication
+ * by 2 to know a circle progress */
+#define CIRCLES_PROGRESS_INTERVAL (0.5 / N_CIRCLES)
+#define CIRCLE_PROGRESS(p) (MIN (1., ((gdouble) (p) * 2.)))
 
-static LocatePointerStage stage;
-static GdkWindow *window = NULL;
-static gint cursor_x, cursor_y;
-static guint locate_pointer_id = 0;
+typedef struct GsdLocatePointerData GsdLocatePointerData;
 
-static gint
-locate_pointer_expose (GtkWidget      *widget,
-                       GdkEventExpose *event,
-                       gpointer        data)
+struct GsdLocatePointerData
 {
-  gint size;
-  GdkPoint points[4];
+  GsdTimeline *timeline;
+  GtkWidget *widget; 
+  GdkWindow *window;
 
-  if (event->window != window)
-    return FALSE;
+  gdouble progress;
+};
 
-  switch (stage)
-    {
-    case STAGE_ONE:
-    case STAGE_TWO:
-      size = LARGE_SIZE;
-      break;
-    case STAGE_THREE:
-    case STAGE_FOUR:
-      size = SMALL_SIZE;
-      break;
-    default:
-      return FALSE;
-    }
-
-  gdk_draw_rectangle (event->window,
-                      widget->style->black_gc,
-                      TRUE,
-                      0, 0, size, size);
-  switch (stage)
-    {
-    case STAGE_ONE:
-    case STAGE_THREE:
-      gdk_draw_rectangle (event->window,
-                          widget->style->white_gc,
-                          FALSE,
-                          1, 1, size - 3, size - 3);
-      break;
-    case STAGE_TWO:
-    case STAGE_FOUR:
-      points[0].x = size/2;
-      points[0].y = 0 + 1;
-      points[1].x = size - 2;
-      points[1].y = size/2;
-      points[2].x = size/2;
-      points[2].y = size - 2;
-      points[3].x = 0 + 1;
-      points[3].y = size/2;
-      gdk_draw_polygon (event->window,
-                        widget->style->white_gc,
-                        FALSE, points, 4);
-      break;
-    default:
-      g_assert_not_reached ();
-    }
-
-  return TRUE;
-}
+static GsdLocatePointerData *data = NULL;
 
 static void
-setup_window (void)
+locate_pointer_paint (GsdLocatePointerData *data,
+		      cairo_t              *cr,
+		      gboolean              composite)
 {
-  gint size;
-  GdkBitmap *mask;
-  GdkGC *gc;
-  GdkColor col;
-  GdkPoint points[4];
+  GdkColor color;
+  gdouble progress, circle_progress;
+  gint width, height, i;
 
-  gdk_window_hide (window);
-  switch (stage)
+  progress = data->progress;
+  gdk_drawable_get_size (data->window, &width, &height);
+  color = data->widget->style->bg[GTK_STATE_SELECTED];
+
+  cairo_set_source_rgba (cr, 1., 1., 1., 0.);
+  cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
+  cairo_paint (cr);
+
+  for (i = 0; i <= N_CIRCLES; i++)
     {
-    case STAGE_ONE:
-    case STAGE_TWO:
-      size = LARGE_SIZE;
-      break;
-    case STAGE_THREE:
-    case STAGE_FOUR:
-      size = SMALL_SIZE;
-      break;
-    default:
-      return;
+      if (progress < 0.)
+	break;
+
+      circle_progress = MIN (1., (progress * 2));
+      progress -= CIRCLES_PROGRESS_INTERVAL;
+
+      if (circle_progress >= 1.)
+	continue;
+
+      if (composite)
+	{
+	  cairo_set_source_rgba (cr,
+				 color.red / 65535.,
+				 color.green / 65535.,
+				 color.blue / 65535.,
+				 1 - circle_progress);
+	  cairo_arc (cr,
+		     width / 2,
+		     height / 2,
+		     circle_progress * width / 2,
+		     0, 2 * G_PI);
+
+	  cairo_fill (cr);
+	  cairo_stroke (cr);
+	}
+      else
+	{
+	  cairo_set_source_rgb (cr, 0., 0., 0.);
+	  cairo_set_line_width (cr, 3.);
+	  cairo_arc (cr,
+		     width / 2,
+		     height / 2,
+		     circle_progress * width / 2,
+		     0, 2 * G_PI);
+	  cairo_stroke (cr);
+
+	  cairo_set_source_rgb (cr, 1., 1., 1.);
+	  cairo_set_line_width (cr, 1.);
+	  cairo_arc (cr,
+		     width / 2,
+		     height / 2,
+		     circle_progress * width / 2,
+		     0, 2 * G_PI);
+	  cairo_stroke (cr);
+	}
     }
-
-  gdk_window_move_resize (window,
-                          cursor_x - size/2,
-                          cursor_y - size/2,
-                          size, size);
-  mask = gdk_pixmap_new (window, size, size, 1);
-  gc = gdk_gc_new (mask);
-  switch (stage)
-    {
-    case STAGE_ONE:
-    case STAGE_THREE:
-      col.pixel = 1;
-      gdk_gc_set_foreground (gc, &col);
-      gdk_draw_rectangle (mask, gc, TRUE, 0, 0, size, size);
-      col.pixel = 0;
-      gdk_gc_set_foreground (gc, &col);
-      gdk_draw_rectangle (mask, gc, TRUE, 3, 3, size - 6, size - 6);
-      break;
-    case STAGE_TWO:
-    case STAGE_FOUR:
-      col.pixel = 0;
-      gdk_gc_set_foreground (gc, &col);
-      gdk_draw_rectangle (mask, gc, TRUE, 0, 0, size, size);
-      col.pixel = 1;
-      gdk_gc_set_foreground (gc, &col);
-      points[0].x = size/2;
-      points[0].y = 0;
-      points[1].x = size - 1;
-      points[1].y = size/2;
-      points[2].x = size/2;
-      points[2].y = size - 1;
-      points[3].x = 0;
-      points[3].y = size/2;
-      gdk_draw_polygon (mask, gc, FALSE, points, 4);
-      points[0].x = size/2;
-      points[0].y = 0 + 1;
-      points[1].x = size - 2;
-      points[1].y = size/2;
-      points[2].x = size/2;
-      points[2].y = size - 2;
-      points[3].x = 0 + 1;
-      points[3].y = size/2;
-      gdk_draw_polygon (mask, gc, FALSE, points, 4);
-      points[0].x = size/2;
-      points[0].y = 0 + 2;
-      points[1].x = size - 3;
-      points[1].y = size/2;
-      points[2].x = size/2;
-      points[2].y = size - 3;
-      points[3].x = 0 + 2;
-      points[3].y = size/2;
-      gdk_draw_polygon (mask, gc, FALSE, points, 4);
-      break;
-    default:
-      g_assert_not_reached ();
-    }
-
-  gdk_window_shape_combine_mask (window, mask, 0, 0);
-  g_object_unref (G_OBJECT (gc));
-  g_object_unref (G_OBJECT (mask));
-  gdk_window_show (window);
-}
-
-static void
-create_window (GdkScreen *screen)
-{
-  GdkWindowAttr attributes;
-  GtkWidget *invisible;
-
-  invisible = gtk_invisible_new_for_screen (screen);
-
-  attributes.window_type = GDK_WINDOW_TEMP;
-  attributes.wclass = GDK_INPUT_OUTPUT;
-  attributes.visual = gtk_widget_get_visual (invisible);
-  attributes.colormap = gtk_widget_get_colormap (invisible);
-  attributes.event_mask = GDK_VISIBILITY_NOTIFY_MASK | GDK_EXPOSURE_MASK;
-  attributes.width = 1;
-  attributes.height = 1;
-  window = gdk_window_new (gdk_screen_get_root_window (screen),
-                           &attributes,
-                           GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL | GDK_WA_COLORMAP);
-  gdk_window_set_user_data (window, invisible);
-  g_signal_connect (G_OBJECT (invisible),
-                    "expose_event",
-                    (GCallback) locate_pointer_expose,
-                    NULL);
 }
 
 static gboolean
-locate_pointer_timeout (gpointer data)
+locate_pointer_expose (GtkWidget *widget,
+		       GdkEventExpose *event,
+		       gpointer        user_data)
 {
-  stage++;
-  if (stage == STAGE_DONE)
+  GsdLocatePointerData *data = (GsdLocatePointerData *) user_data;
+  cairo_t *cr;
+  GdkBitmap *mask;
+
+  if (event->window != data->window)
+    return FALSE;
+
+  cr = gdk_cairo_create (data->window);
+
+  if (gtk_widget_is_composited (data->widget))
+    locate_pointer_paint (data, cr, TRUE);
+  else
     {
-      gdk_window_hide (window);
-      locate_pointer_id = 0;
-      return FALSE;
+      locate_pointer_paint (data, cr, FALSE);
+      cairo_destroy (cr);
+
+      /* create a bitmap for the shape, reuse the cairo_t to paint on it */
+      mask = gdk_pixmap_new (data->window, WINDOW_SIZE, WINDOW_SIZE, 1);
+      cr = gdk_cairo_create (mask);
+      locate_pointer_paint (data, cr, FALSE);
+      gdk_window_shape_combine_mask (data->window, mask, 0, 0);
     }
-  setup_window ();
+
+  cairo_destroy (cr);
+
   return TRUE;
+}
+
+static void
+timeline_frame_cb (GtkTimeline *timeline,
+		   gdouble      progress,
+		   gpointer     user_data)
+{
+  GsdLocatePointerData *data = (GsdLocatePointerData *) user_data;
+
+  if (gtk_widget_is_composited (data->widget))
+    {
+      gdk_window_invalidate_rect (data->window, NULL, FALSE);
+      data->progress = progress;
+    }
+  else if (progress >= data->progress + CIRCLES_PROGRESS_INTERVAL)
+    {
+      /* only invalidate window each circle interval */
+      gdk_window_invalidate_rect (data->window, NULL, FALSE);
+      data->progress += CIRCLES_PROGRESS_INTERVAL;
+    }
+}
+
+static void
+timeline_finished_cb (GtkTimeline *timeline,
+		      gpointer     user_data)
+{
+  GsdLocatePointerData *data = (GsdLocatePointerData *) user_data;
+
+  /* hide window and unset shape */
+  gdk_window_hide (data->window);
+  gdk_window_shape_combine_mask (data->window, NULL, 0, 0);
+}
+
+static void
+create_window (GsdLocatePointerData *data,
+	       GdkScreen            *screen)
+{
+  GdkColormap *colormap;
+  GdkVisual *visual;
+  GdkWindowAttr attributes;
+
+  colormap = gdk_screen_get_rgba_colormap (screen);
+  visual = gdk_screen_get_rgba_visual (screen);
+
+  if (!colormap)
+    {
+      colormap = gdk_screen_get_rgb_colormap (screen);
+      visual = gdk_screen_get_rgb_visual (screen);
+    }
+
+  attributes.window_type = GDK_WINDOW_TEMP;
+  attributes.wclass = GDK_INPUT_OUTPUT;
+  attributes.visual = visual;
+  attributes.colormap = colormap;
+  attributes.event_mask = GDK_VISIBILITY_NOTIFY_MASK | GDK_EXPOSURE_MASK;
+  attributes.width = 1;
+  attributes.height = 1;
+
+  data->window = gdk_window_new (gdk_screen_get_root_window (screen),
+				 &attributes,
+				 GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL | GDK_WA_COLORMAP);
+
+  gdk_window_set_user_data (data->window, data->widget);
+}
+
+static GsdLocatePointerData *
+gsd_locate_pointer_data_new (GdkScreen *screen)
+{
+  GsdLocatePointerData *data;
+
+  data = g_new0 (GsdLocatePointerData, 1);
+
+  /* this widget will never be shown, it's
+   * mainly used to get signals/events from
+   */
+  data->widget = gtk_window_new (GTK_WINDOW_POPUP);
+  gtk_widget_realize (data->widget);
+
+  g_signal_connect (G_OBJECT (data->widget), "expose_event",
+                    G_CALLBACK (locate_pointer_expose),
+                    data);
+
+  data->timeline = gsd_timeline_new (ANIMATION_LENGTH);
+  g_signal_connect (data->timeline, "frame",
+		    G_CALLBACK (timeline_frame_cb), data);
+  g_signal_connect (data->timeline, "finished",
+		    G_CALLBACK (timeline_finished_cb), data);
+
+  create_window (data, screen);
+
+  return data;
+}
+
+static void
+move_locate_pointer_window (GsdLocatePointerData *data,
+			    GdkScreen            *screen)
+{
+  gint cursor_x, cursor_y;
+  GdkBitmap *mask;
+  GdkColor col;
+  GdkGC *gc;
+
+  gdk_window_get_pointer (gdk_screen_get_root_window (screen), &cursor_x, &cursor_y, NULL);
+
+  gdk_window_move_resize (data->window,
+                          cursor_x - WINDOW_SIZE / 2,
+                          cursor_y - WINDOW_SIZE / 2,
+                          WINDOW_SIZE, WINDOW_SIZE);
+
+  col.pixel = 0;
+  mask = gdk_pixmap_new (data->window, WINDOW_SIZE, WINDOW_SIZE, 1);
+
+  gc = gdk_gc_new (mask);
+  gdk_gc_set_foreground (gc, &col);
+  gdk_draw_rectangle (mask, gc, TRUE, 0, 0, WINDOW_SIZE, WINDOW_SIZE);
+
+  /* allow events to happen through the window */
+  gdk_window_input_shape_combine_mask (data->window, mask, 0, 0);
+
+  g_object_unref (mask);
+  g_object_unref (gc);
 }
 
 void
 gsd_locate_pointer (GdkScreen *screen)
 {
-  gdk_window_get_pointer (gdk_screen_get_root_window (screen), &cursor_x, &cursor_y, NULL);
+  if (!data)
+    data = gsd_locate_pointer_data_new (screen);
 
-  if (locate_pointer_id)
-    gtk_timeout_remove (locate_pointer_id);
+  gsd_timeline_pause (data->timeline);
+  gsd_timeline_rewind (data->timeline);
 
-  /* Create the window if it is not created OR if it is not for the
-   * current screen.
-   */
+  /* Create again the window if it is not for the current screen */
+  if (gdk_screen_get_number (screen) != gdk_screen_get_number (gdk_drawable_get_screen (data->window)))
+    {
+      gdk_window_set_user_data (data->window, NULL);
+      gdk_window_destroy (data->window);
 
-  if (window == NULL)
-    create_window (screen);
-  else if( gdk_screen_get_number (screen) != gdk_screen_get_number (gdk_drawable_get_screen (window)))
-    create_window (screen);
+      create_window (data, screen);
+    }
 
-  stage = STAGE_ONE;
-  setup_window ();
-  gdk_window_show (window);
-  locate_pointer_id = gtk_timeout_add (100, locate_pointer_timeout, NULL);
+  data->progress = 0.;
+  gdk_window_show (data->window);
+  move_locate_pointer_window (data, screen);
+
+  gsd_timeline_start (data->timeline);
 }
