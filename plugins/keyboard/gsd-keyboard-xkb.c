77c3693680d94f3f73276698604675dafdc0f97e
Sergey V. Udaltsov <svu@gnome.org>
2009-07-01 16:13:50 +0100

    Depend on libxklavier 4.0
    The API was changed, all related gnome modules updated

--- plugins/keyboard/gsd-keyboard-xkb.c
+++ plugins/keyboard/gsd-keyboard-xkb.c
@@ -168,7 +168,8 @@ filter_xkb_config (void)
 	xkl_debug (100, "Filtering configuration against the registry\n");
 	if (!xkl_registry) {
 		xkl_registry = xkl_config_registry_get_instance (xkl_engine);
-		if (!xkl_config_registry_load (xkl_registry)) {
+		/* load all materials, unconditionally! */
+		if (!xkl_config_registry_load (xkl_registry, TRUE)) {
 			g_object_unref (xkl_registry);
 			xkl_registry = NULL;
 			return FALSE;
