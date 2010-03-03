7e745cf7d5ce95413b085572d3ad591bd11aab38
Federico Mena Quintero <federico@novell.com>
2010-03-03 12:50:06 -0600

    Let the RANDR OSD focus itself when it is mapped
    It is meant to be an interactive window, so we need the input focus.
    
    Signed-off-by: Federico Mena Quintero <federico@novell.com>

--- plugins/xrandr/gsd-xrandr-manager.c
+++ plugins/xrandr/gsd-xrandr-manager.c
@@ -1205,8 +1205,10 @@ create_osd_window (GsdXrandrManager *manager)
                 destroy_osd_window (manager);
 
         priv->osd_window = gsd_osd_window_new ();
+
         gsd_osd_window_set_auto_hide (GSD_OSD_WINDOW (priv->osd_window), FALSE);
         gtk_window_set_type_hint (GTK_WINDOW (priv->osd_window), GDK_WINDOW_TYPE_HINT_NORMAL);
+        gtk_window_set_focus_on_map (GTK_WINDOW (priv->osd_window), TRUE);
 
         box = gtk_hbox_new (TRUE, 12);
         gtk_container_add (GTK_CONTAINER (priv->osd_window), box);
