2feea4a4d40bac99d9990449890cf2fb69da5aa8
William Jon McCann <jmccann@redhat.com>
2010-11-12 12:02:45 -0500

    media-keys: Fix crash from incorrect error checking
    Don't check the res value to determine whether an error occurred.
    According to the documentation one should check the variant == NULL
    or error != NULL.  Went with the latter.

--- plugins/media-keys/gsd-media-keys-manager.c
+++ plugins/media-keys/gsd-media-keys-manager.c
@@ -952,27 +952,27 @@ do_multimedia_player_action (GsdMediaKeysManager *manager,
 
 static void
 on_xrandr_action_call_finished (GObject             *source_object,
-				GAsyncResult        *res,
-				GsdMediaKeysManager *manager)
+                                GAsyncResult        *res,
+                                GsdMediaKeysManager *manager)
 {
         GError *error = NULL;
         GVariant *variant;
         char *action;
 
         action = g_object_get_data (G_OBJECT (source_object),
-				    "gsd-media-keys-manager-xrandr-action");
+                                    "gsd-media-keys-manager-xrandr-action");
 
-	variant = g_dbus_proxy_call_finish (G_DBUS_PROXY (source_object), res, &error);
+        variant = g_dbus_proxy_call_finish (G_DBUS_PROXY (source_object), res, &error);
 
-	g_object_unref (manager->priv->cancellable);
-	manager->priv->cancellable = NULL;
+        g_object_unref (manager->priv->cancellable);
+        manager->priv->cancellable = NULL;
 
-        if (!res) {
+        if (error != NULL) {
                 g_warning ("Unable to call '%s': %s", action, error->message);
                 g_error_free (error);
         } else {
-		g_variant_unref (variant);
-	}
+                g_variant_unref (variant);
+        }
 
         g_free (action);
 }
