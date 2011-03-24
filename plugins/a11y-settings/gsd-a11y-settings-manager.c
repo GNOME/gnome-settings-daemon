e64d7747d55e25e1af82720ebb4f4b4f8b2560f1
Bastien Nocera <hadess@hadess.net>
2011-03-24 17:08:34 +0000

    a11y-settings: Set toolkit-accessibility to on for either
    If *either* the on-screen keyboard or the screen reader are
    enabled we need to enable "toolkit-accessibility"
    
    https://bugzilla.gnome.org/show_bug.cgi?id=645721

--- plugins/a11y-settings/gsd-a11y-settings-manager.c
+++ plugins/a11y-settings/gsd-a11y-settings-manager.c
@@ -70,13 +70,18 @@ apps_settings_changed (GSettings              *settings,
 	    g_str_equal (key, "screen-keyboard-enabled") == FALSE)
 		return;
 
+	g_debug ("screen reader or OSK enablement changed");
+
 	screen_reader = g_settings_get_boolean (manager->priv->a11y_apps_settings, "screen-reader-enabled");
 	keyboard = g_settings_get_boolean (manager->priv->a11y_apps_settings, "screen-keyboard-enabled");
 
-	if (screen_reader && keyboard)
+	if (screen_reader || keyboard) {
+		g_debug ("Enabling toolkit-accessibility, screen reader or OSK enabled");
 		g_settings_set_boolean (manager->priv->interface_settings, "toolkit-accessibility", TRUE);
-	else if (screen_reader == FALSE && keyboard == FALSE)
+	} else if (screen_reader == FALSE && keyboard == FALSE) {
+		g_debug ("Disabling toolkit-accessibility, screen reader and OSK disabled");
 		g_settings_set_boolean (manager->priv->interface_settings, "toolkit-accessibility", FALSE);
+	}
 }
 
 gboolean
