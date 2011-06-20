ac9e47c684f17ad9b90a4cccc8458852c388b0c0
Bastien Nocera <hadess@hadess.net>
2011-06-20 10:26:59 +0100

    keyboard: Add missing "Settings" to the string

--- plugins/keyboard/gsd-keyboard-xkb.c
+++ plugins/keyboard/gsd-keyboard-xkb.c
@@ -254,7 +254,7 @@ status_icon_popup_menu_cb (GtkStatusIcon * icon, guint button, guint time)
 
 	/* translators note:
 	 * This is the name of the gnome-control-center "region" panel */
-	item = gtk_menu_item_new_with_mnemonic (_("Region and Language"));
+	item = gtk_menu_item_new_with_mnemonic (_("Region and Language Settings"));
 	gtk_widget_show (item);
 	g_signal_connect (item, "activate", popup_menu_show_layout, NULL);
 	gtk_menu_shell_append (GTK_MENU_SHELL (popup_menu), item);
