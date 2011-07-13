7687467d6bbe115ff909f72a259476effbb57194
Rodrigo Moya <rodrigo@gnome-db.org>
2011-07-13 20:07:08 +0200

    common: Fix grabbing of multimedia keys

--- plugins/common/gsd-keygrab.c
+++ plugins/common/gsd-keygrab.c
@@ -146,9 +146,9 @@ grab_key_unsafe (Key                 *key,
          * (which are useful to grab without a modifier).
          */
         if ((modifiers & gsd_used_mods) == 0 &&
-            (IN_RANGE(key->keysym, XF86KEYS_RANGE_MIN, XF86KEYS_RANGE_MAX) ||
-             IN_RANGE(key->keysym, FKEYS_RANGE_MIN, FKEYS_RANGE_MAX) ||
-             key->keysym == GDK_KEY_Pause)) {
+            !IN_RANGE(key->keysym, XF86KEYS_RANGE_MIN, XF86KEYS_RANGE_MAX) &&
+            !IN_RANGE(key->keysym, FKEYS_RANGE_MIN, FKEYS_RANGE_MAX) &&
+             key->keysym != GDK_KEY_Pause) {
                 GString *keycodes;
 
                 keycodes = g_string_new ("");
