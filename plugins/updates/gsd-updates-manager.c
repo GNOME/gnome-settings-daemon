f95736b80dd621fafc35bf8e3aa481e328897545
Richard Hughes <richard@hughsie.com>
2011-09-06 17:09:52 +0100

    updates: Ignore virtual mountpoints when looking for external media
    Resolves https://bugzilla.gnome.org/show_bug.cgi?id=658282

--- plugins/updates/gsd-updates-manager.c
+++ plugins/updates/gsd-updates-manager.c
@@ -1176,12 +1176,16 @@ update_viewer_appeared_cb (GDBusConnection *connection,
 static gboolean
 file_exists_in_root (const gchar *root, const gchar *filename)
 {
-        gboolean ret;
+        gboolean ret = FALSE;
         GFile *source;
         gchar *source_path;
 
         source_path = g_build_filename (root, filename, NULL);
         source = g_file_new_for_path (source_path);
+
+        /* ignore virtual mountpoints */
+        if (!g_file_is_native (source))
+                goto out;
 
         /* an interesting file exists */
         ret = g_file_query_exists (source, NULL);
