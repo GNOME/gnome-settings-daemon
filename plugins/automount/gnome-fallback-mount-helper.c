35cda8c423f3c9639ff63c15a5432c9ea03c4282
Bastien Nocera <hadess@hadess.net>
2011-10-07 22:02:07 +0100

    automount: Call bind_textdomain_codeset()
    Which should avoid some problems in non-UTF-8 locales.

--- plugins/automount/gnome-fallback-mount-helper.c
+++ plugins/automount/gnome-fallback-mount-helper.c
@@ -40,6 +40,7 @@ main (int argc,
         gtk_init (&argc, &argv);
 
         bindtextdomain (GETTEXT_PACKAGE, GNOME_SETTINGS_LOCALEDIR);
+        bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
         textdomain (GETTEXT_PACKAGE);
 
         loop = g_main_loop_new (NULL, FALSE);
