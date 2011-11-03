ac0a7b45a77ad6a6f031ca5e06c8797367f13a53
Bastien Nocera <hadess@hadess.net>
2011-11-03 17:12:03 +0000

    common: Remove egg copy/paste code
    https://bugzilla.gnome.org/show_bug.cgi?id=663343

--- plugins/common/test-egg-key-parsing.c
+++ plugins/common/test-egg-key-parsing.c
@@ -1,24 +1,16 @@
 #include <gtk/gtk.h>
-#include "eggaccelerators.h"
 
 #define KEY "<Alt>XF86AudioMute"
 
 int main (int argc, char **argv)
 {
-	guint gdk_accel_key, egg_accel_key;
-	guint *gdk_accel_codes, *egg_accel_codes;
+	guint gdk_accel_key;
+	guint *gdk_accel_codes;
 	GdkModifierType gdk_mods;
-	EggVirtualModifierType egg_mods;
-	EggParseError retval;
 
 	gtk_init (&argc, &argv);
 
 	g_message ("gdk_keyval_from_name ('%s') == %d", KEY, gdk_keyval_from_name(KEY));
-
-	retval = egg_accelerator_parse_virtual (KEY, &egg_accel_key, &egg_accel_codes, &egg_mods);
-	g_message ("egg_accelerator_parse_virtual ('%s') returned keyval '%d' keycode[0]: '%d' mods: 0x%x (%s)",
-		   KEY, egg_accel_key, egg_accel_codes ? egg_accel_codes[0] : 0, egg_mods,
-		   (retval == EGG_PARSE_ERROR_NONE) ? "success" : "failure");
 
 	gtk_accelerator_parse_with_keycode (KEY, &gdk_accel_key, &gdk_accel_codes, &gdk_mods);
 	g_message ("gtk_accelerator_parse_full ('%s') returned keyval '%d' keycode[0]: '%d' mods: 0x%x",
