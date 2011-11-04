7d49f832403ba74697a8e9b9b100642e35bb99a1
Bastien Nocera <hadess@hadess.net>
2011-11-04 16:55:21 +0000

    common: Fix small memleak

--- plugins/common/test-egg-key-parsing.c
+++ plugins/common/test-egg-key-parsing.c
@@ -15,6 +15,7 @@ int main (int argc, char **argv)
 	gtk_accelerator_parse_with_keycode (KEY, &gdk_accel_key, &gdk_accel_codes, &gdk_mods);
 	g_message ("gtk_accelerator_parse_full ('%s') returned keyval '%d' keycode[0]: '%d' mods: 0x%x",
 		   KEY, gdk_accel_key, gdk_accel_codes ? gdk_accel_codes[0] : 0, gdk_mods);
+	g_free (gdk_accel_codes);
 
 	return 0;
 }
