7386e5cf33da16ea813abfd920bbc577ace54ae0
Bastien Nocera <hadess@hadess.net>
2011-12-14 14:36:01 +0000

    orientation: Remove ifdef's for i18n support

--- plugins/orientation/test-orientation.c
+++ plugins/orientation/test-orientation.c
@@ -37,13 +37,9 @@ main (int    argc,
 {
         GError  *error;
 
-#ifdef ENABLE_NLS
         bindtextdomain (GETTEXT_PACKAGE, GNOME_SETTINGS_LOCALEDIR);
-# ifdef HAVE_BIND_TEXTDOMAIN_CODESET
         bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-# endif
         textdomain (GETTEXT_PACKAGE);
-#endif
 
         error = NULL;
         if (! gtk_init_with_args (&argc, &argv, NULL, NULL, NULL, &error)) {
