c1d43558f207bb7244180f2f031b1fef28611ff3
Richard Hughes <richard@hughsie.com>
2012-03-05 18:05:58 +0000

    color: Fix up make check so the checksums match reality

--- plugins/color/gcm-self-test.c
+++ plugins/color/gcm-self-test.c
@@ -65,7 +65,7 @@ gcm_test_edid_func (void)
         g_assert_cmpstr (gcm_edid_get_vendor_name (edid), ==, "Goldstar Company Ltd");
         g_assert_cmpstr (gcm_edid_get_serial_number (edid), ==, "34398");
         g_assert_cmpstr (gcm_edid_get_eisa_id (edid), ==, NULL);
-        g_assert_cmpstr (gcm_edid_get_checksum (edid), ==, "80b7dda4c74b06366abb8fa23e71d645");
+        g_assert_cmpstr (gcm_edid_get_checksum (edid), ==, "0bb44865bb29984a4bae620656c31368");
         g_assert_cmpstr (gcm_edid_get_pnp_id (edid), ==, "GSM");
         g_assert_cmpint (gcm_edid_get_height (edid), ==, 30);
         g_assert_cmpint (gcm_edid_get_width (edid), ==, 47);
@@ -86,7 +86,7 @@ gcm_test_edid_func (void)
         g_assert_cmpstr (gcm_edid_get_vendor_name (edid), ==, "IBM France");
         g_assert_cmpstr (gcm_edid_get_serial_number (edid), ==, NULL);
         g_assert_cmpstr (gcm_edid_get_eisa_id (edid), ==, "LTN154P2-L05");
-        g_assert_cmpstr (gcm_edid_get_checksum (edid), ==, "c585d9e80adc65c54f0a52597e850f83");
+        g_assert_cmpstr (gcm_edid_get_checksum (edid), ==, "e1865128c7cd5e5ed49ecfc8102f6f9c");
         g_assert_cmpstr (gcm_edid_get_pnp_id (edid), ==, "IBM");
         g_assert_cmpint (gcm_edid_get_height (edid), ==, 21);
         g_assert_cmpint (gcm_edid_get_width (edid), ==, 33);
