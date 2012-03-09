3fb981fc0f994adfe580df91450bb8befcbe8f4f
Ryan Lortie <desrt@desrt.ca>
2012-03-09 12:12:19 -0500

    xsettings: i18nify the test-xsettings program

--- plugins/xsettings/test-xsettings.c
+++ plugins/xsettings/test-xsettings.c
@@ -1,4 +1,7 @@
+#include "config.h"
+
 #include "gsd-xsettings-manager.h"
+#include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
 int
@@ -6,6 +9,10 @@ main (void)
 {
   GnomeXSettingsManager *manager;
   GError *error = NULL;
+
+  bindtextdomain (GETTEXT_PACKAGE, GNOME_SETTINGS_LOCALEDIR);
+  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+  textdomain (GETTEXT_PACKAGE);
 
   gtk_init (NULL, NULL);
 
