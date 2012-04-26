df7866b531c9b9cd93c366ba728ceb20a8cd688a
Bastien Nocera <hadess@hadess.net>
2012-04-26 17:20:40 +0100

    orientation: Simplify test app

--- plugins/orientation/test-orientation.c
+++ plugins/orientation/test-orientation.c
@@ -1,59 +1,6 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
- *
- * Copyright (C) 2007 William Jon McCann <mccann@jhu.edu>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
- *
- *
- */
-
-#include "config.h"
-
-#include <stdlib.h>
-
-#include <glib/gi18n.h>
-#include <gtk/gtk.h>
-
+#define NEW gsd_orientation_manager_new
+#define START gsd_orientation_manager_start
+#define MANAGER GsdOrientationManager
 #include "gsd-orientation-manager.h"
 
-static GsdOrientationManager *manager = NULL;
-
-int
-main (int    argc,
-      char **argv)
-{
-        GError  *error;
-
-        bindtextdomain (GETTEXT_PACKAGE, GNOME_SETTINGS_LOCALEDIR);
-        bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-        textdomain (GETTEXT_PACKAGE);
-
-        error = NULL;
-        if (! gtk_init_with_args (&argc, &argv, NULL, NULL, NULL, &error)) {
-                fprintf (stderr, "%s", error->message);
-                g_error_free (error);
-                exit (1);
-        }
-
-        manager = gsd_orientation_manager_new ();
-
-        error = NULL;
-        gsd_orientation_manager_start (manager, &error);
-
-        gtk_main ();
-
-        return 0;
-}
+#include "test-plugin.h"
