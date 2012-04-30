6244a2acb9fbff20ec94b1cc4cb36b7cb50ff2a5
William Jon McCann <jmccann@redhat.com>
2012-04-30 12:49:04 -0400

    Support new XDG thumbnail directory locations
    https://bugzilla.gnome.org/show_bug.cgi?id=675166

--- plugins/housekeeping/gsd-housekeeping-manager.c
+++ plugins/housekeeping/gsd-housekeeping-manager.c
@@ -155,6 +155,27 @@ get_thumbnail_dirs (void)
 
         array = g_ptr_array_new ();
 
+        /* check new XDG cache */
+        path = g_build_filename (g_get_user_cache_dir (),
+                                 "thumbnails",
+                                 "normal",
+                                 NULL);
+        g_ptr_array_add (array, path);
+
+        path = g_build_filename (g_get_user_cache_dir (),
+                                 "thumbnails",
+                                 "large",
+                                 NULL);
+        g_ptr_array_add (array, path);
+
+        path = g_build_filename (g_get_user_cache_dir (),
+                                 "thumbnails",
+                                 "fail",
+                                 "gnome-thumbnail-factory",
+                                 NULL);
+        g_ptr_array_add (array, path);
+
+        /* cleanup obsolete locations too */
         path = g_build_filename (g_get_home_dir (),
                                  ".thumbnails",
                                  "normal",
