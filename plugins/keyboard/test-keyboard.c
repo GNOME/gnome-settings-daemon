b104ce2c61f93039a990711c5ec2ac10d2d31538
Bastien Nocera <hadess@hadess.net>
2012-05-25 13:59:37 +0100

    keyboard: Update for test-plugin.h change

--- plugins/keyboard/test-keyboard.c
+++ plugins/keyboard/test-keyboard.c
@@ -1,5 +1,6 @@
 #define NEW gsd_keyboard_manager_new
 #define START gsd_keyboard_manager_start
+#define STOP gsd_keyboard_manager_stop
 #define MANAGER GsdKeyboardManager
 #include "gsd-keyboard-manager.h"
 
