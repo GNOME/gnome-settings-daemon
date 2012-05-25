a7d23b2d415bb6ae6481925d6b8c95cb66ba0c59
Bastien Nocera <hadess@hadess.net>
2012-05-25 13:59:26 +0100

    print-notifications: Update for test-plugin.h change

--- plugins/print-notifications/test-print-notifications.c
+++ plugins/print-notifications/test-print-notifications.c
@@ -1,5 +1,6 @@
 #define NEW gsd_print_notifications_manager_new
 #define START gsd_print_notifications_manager_start
+#define STOP gsd_print_notifications_manager_stop
 #define MANAGER GsdPrintNotificationsManager
 #include "gsd-print-notifications-manager.h"
 
