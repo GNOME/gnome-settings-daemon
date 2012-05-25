d18d91ce7bd2a61997f8c778b01c806e468b81c7
Bastien Nocera <hadess@hadess.net>
2012-05-25 13:59:16 +0100

    background: Update for test-plugin.h change

--- plugins/background/test-background.c
+++ plugins/background/test-background.c
@@ -1,5 +1,6 @@
 #define NEW gsd_background_manager_new
 #define START gsd_background_manager_start
+#define STOP gsd_background_manager_stop
 #define MANAGER GsdBackgroundManager
 #include "gsd-background-manager.h"
 
