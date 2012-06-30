9f951b2d6b9ebe7057f78415c55eab0e95893c7f
Matthias Clasen <mclasen@redhat.com>
2012-06-29 23:58:04 -0400

    housekeeping: Don't leak notification objects
    https://bugzilla.gnome.org/show_bug.cgi?id=679177

--- plugins/housekeeping/gsd-disk-space.c
+++ plugins/housekeeping/gsd-disk-space.c
@@ -292,7 +292,7 @@ empty_trash_callback (NotifyNotification *n,
 static void
 on_notification_closed (NotifyNotification *notification)
 {
-        notification = NULL;
+        g_object_unref (notification);
 }
 
 static gboolean
