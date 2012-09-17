d1285e0f7cfacc3c2ebacd741782f0c70b466ed8
Bastien Nocera <hadess@hadess.net>
2012-09-17 12:32:02 +0100

    keyboard: More num-lock debugging

--- plugins/keyboard/gsd-keyboard-manager.c
+++ plugins/keyboard/gsd-keyboard-manager.c
@@ -946,13 +946,16 @@ apply_numlock (GsdKeyboardManager *manager)
         gboolean rnumlock;
 
         g_debug ("Applying the num-lock settings");
-        settings      = manager->priv->settings;
+        settings = manager->priv->settings;
         rnumlock = g_settings_get_boolean  (settings, KEY_REMEMBER_NUMLOCK_STATE);
         manager->priv->old_state = g_settings_get_enum (manager->priv->settings, KEY_NUMLOCK_STATE);
 
         gdk_error_trap_push ();
-        if (rnumlock)
+        if (rnumlock) {
+                g_debug ("Remember num-lock is set, so applying setting '%s'",
+                         num_lock_state_to_string (manager->priv->old_state));
                 numlock_set_xkb_state (manager->priv->old_state);
+        }
 
         XSync (GDK_DISPLAY_XDISPLAY (gdk_display_get_default ()), FALSE);
         gdk_error_trap_pop_ignored ();
