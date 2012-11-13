6f7429d86d314a5ba22538ec14a092ea64b83cbc
Bastien Nocera <hadess@hadess.net>
2012-11-13 10:40:33 +0100

    cursor: Add test program

---
+++ plugins/cursor/test-cursor.c
@@ -0,0 +1,7 @@
+#define NEW gsd_cursor_manager_new
+#define START gsd_cursor_manager_start
+#define STOP gsd_cursor_manager_stop
+#define MANAGER GsdCursorManager
+#include "gsd-cursor-manager.h"
+
+#include "test-plugin.h"
