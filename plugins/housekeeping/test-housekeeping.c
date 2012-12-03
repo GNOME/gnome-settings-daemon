19a073a69097a35e4e4416e6b762694c89d45df1
Bastien Nocera <hadess@hadess.net>
2012-12-03 10:56:02 +0100

    housekeeping: Add test application

---
+++ plugins/housekeeping/test-housekeeping.c
@@ -0,0 +1,7 @@
+#define NEW gsd_housekeeping_manager_new
+#define START gsd_housekeeping_manager_start
+#define STOP gsd_housekeeping_manager_stop
+#define MANAGER GsdHousekeepingManager
+#include "gsd-housekeeping-manager.h"
+
+#include "test-plugin.h"
