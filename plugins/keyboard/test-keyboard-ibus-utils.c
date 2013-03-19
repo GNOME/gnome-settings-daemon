613f7965d15c764645085287f1dfe0b1903208b5
Rui Matos <tiagomatos@gmail.com>
2013-03-19 15:02:21 +0100

    keyboard: Always try to activate the ibus daemon
    We set XMODIFIERS=@im=ibus globally at session start so that XIM
    clients can immediately benefit from IBus input sources when/if the
    user adds one. Unfortunately, these clients then won't work correctly
    in case we don't actually activate the ibus daemon. E.g. dead keys
    don't work in this case.
    
    Also, previously we'd try to set an "xkb:..." ibus engine according to
    the XKB layout. It happens that this wouldn't work for most layouts
    since there isn't an ibus engine for each layout and it's actually not
    needed since all of those "xkb:..." engines work the same way despite
    having different names.
    
    As such we'll always try to activate the daemon even if none of the
    user's current input sources need it and set one of the trivial "echo"
    engines so that XIM clients work correctly.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=695867

--- plugins/keyboard/test-keyboard-ibus-utils.c
+++ plugins/keyboard/test-keyboard-ibus-utils.c
@@ -1,26 +1,6 @@
 #include "gsd-keyboard-manager.c"
 
 static void
-test_make_xkb_source_id (void)
-{
-        gint i;
-        const gchar *test_strings[][2] = {
-                /* input                output */
-                { "xkb:aa:bb:cc",       "aa+bb" },
-                { "xkb:aa:bb:",         "aa+bb" },
-                { "xkb:aa::cc",         "aa" },
-                { "xkb:aa::",           "aa" },
-                { "xkb::bb:cc",         "+bb" },
-                { "xkb::bb:",           "+bb" },
-                { "xkb:::cc",           "" },
-                { "xkb:::",             "" },
-        };
-
-        for (i = 0; i < G_N_ELEMENTS (test_strings); ++i)
-                g_assert_cmpstr (make_xkb_source_id (test_strings[i][0]), ==, test_strings[i][1]);
-}
-
-static void
 test_layout_from_ibus_layout (void)
 {
         gint i;
@@ -107,7 +87,6 @@ test_options_from_ibus_layout (void)
 int
 main (void)
 {
-        test_make_xkb_source_id ();
         test_layout_from_ibus_layout ();
         test_variant_from_ibus_layout ();
         test_options_from_ibus_layout ();
