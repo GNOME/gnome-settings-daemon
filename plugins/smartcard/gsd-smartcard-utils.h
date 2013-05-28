58320622e2480676ab85d631af287a0af9ffab6e
Ray Strode <rstrode@redhat.com>
2013-05-28 09:22:39 -0400

    smartcard: add back smartcard support
    This commit lands a rewrite of the smartcard plugin.  It makes use
    of the DBus ObjectManager interface to export tokens over the bus,
    as they're inserted and removed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=704890

---
+++ plugins/smartcard/gsd-smartcard-utils.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef __GSD_SMARTCARD_UTILS_H
+#define __GSD_SMARTCARD_UTILS_H
+
+#include <glib-object.h>
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+void             gsd_smartcard_utils_register_error_domain             (GQuark error_domain,
+                                                                        GType error_enum);
+char *           gsd_smartcard_utils_escape_object_path                (const char *unescaped_string);
+
+gboolean         gsd_smartcard_utils_finish_boolean_task               (GObject       *object,
+                                                                        GAsyncResult  *result,
+                                                                        GError       **error);
+
+G_END_DECLS
+
+#endif /* __GSD_SMARTCARD_MANAGER_H */
