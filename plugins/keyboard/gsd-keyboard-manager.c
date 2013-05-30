cb35cb2fc90866423be02bf866f60111887cf96f
Rui Matos <tiagomatos@gmail.com>
2013-05-30 13:52:16 +0200

    keyboard: Wait until fully initialized to show up on the bus
    We don't want to get DBus requests until we applied all settings at
    least once.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=701055

--- plugins/keyboard/gsd-keyboard-manager.c
+++ plugins/keyboard/gsd-keyboard-manager.c
@@ -1595,6 +1595,7 @@ localed_proxy_ready (GObject      *source,
         maybe_create_initial_settings (manager);
 out:
         apply_input_sources_settings (manager->priv->input_sources_settings, NULL, 0, manager);
+        register_manager_dbus (manager);
 }
 
 static gboolean
@@ -1636,7 +1637,6 @@ start_keyboard_idle_cb (GsdKeyboardManager *manager)
                           G_CALLBACK (apply_input_sources_settings), manager);
 
 	install_xkb_filter (manager);
-        register_manager_dbus (manager);
 
         gnome_settings_profile_end (NULL);
 
