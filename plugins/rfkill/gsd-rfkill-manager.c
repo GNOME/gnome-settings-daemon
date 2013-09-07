10c0778f889f3f23b75d1a12bc6a0c74056fb820
Kalev Lember <kalevlember@gmail.com>
2013-09-07 21:04:13 +0200

    rfkill: Remove empty constructor function
    It was just chaining up.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=707705

--- plugins/rfkill/gsd-rfkill-manager.c
+++ plugins/rfkill/gsd-rfkill-manager.c
@@ -63,26 +63,11 @@ G_DEFINE_TYPE (GsdRfkillManager, gsd_rfkill_manager, G_TYPE_OBJECT)
 
 static gpointer manager_object = NULL;
 
-static GObject *
-gsd_rfkill_manager_constructor (GType                     type,
-                               guint                      n_construct_properties,
-                               GObjectConstructParam     *construct_properties)
-{
-        GsdRfkillManager      *rfkill_manager;
-
-        rfkill_manager = GSD_RFKILL_MANAGER (G_OBJECT_CLASS (gsd_rfkill_manager_parent_class)->constructor (type,
-                                                                                                         n_construct_properties,
-                                                                                                         construct_properties));
-
-        return G_OBJECT (rfkill_manager);
-}
-
 static void
 gsd_rfkill_manager_class_init (GsdRfkillManagerClass *klass)
 {
         GObjectClass   *object_class = G_OBJECT_CLASS (klass);
 
-        object_class->constructor = gsd_rfkill_manager_constructor;
         object_class->finalize = gsd_rfkill_manager_finalize;
 
         g_type_class_add_private (klass, sizeof (GsdRfkillManagerPrivate));
