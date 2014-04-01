fc22a8100fa40533fddfb4338160a20188e46c6b
Kalev Lember <kalevlember@gmail.com>
2014-04-01 15:21:24 +0200

    updates: Remove the updates plugin
    The functionality now lives in gnome-software that has its own session
    service for update polling.

--- plugins/updates/test-updates.c
+++
@@ -1,7 +0,0 @@
-#define NEW gsd_updates_manager_new
-#define START gsd_updates_manager_start
-#define STOP gsd_updates_manager_stop
-#define MANAGER GsdUpdatesManager
-#include "gsd-updates-manager.h"
-
-#include "test-plugin.h"
