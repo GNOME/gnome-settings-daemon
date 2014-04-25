41f11e665d94384021f8162c84ae6f7cc82c4dde
Bastien Nocera <hadess@hadess.net>
2014-04-25 15:41:42 +0200

    common: Fix documentation for hotplug script
    The code at the bottom says we'll ignore further configuration if
    "1" is returned, but the top said if "0" is returned.
    
    Make both say "1".

--- plugins/common/input-device-example.sh
+++ plugins/common/input-device-example.sh
@@ -12,7 +12,7 @@
 #       device ID being the XInput device ID
 # <device name> The name of the device
 #
-# The script should return 0 if the device is to be
+# The script should return 1 if the device is to be
 # ignored from future configuration.
 #
 # Set the script to be used with:
