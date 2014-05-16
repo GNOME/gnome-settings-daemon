82c91ff6f929a534e26cb70dbd35d344847feb44
Bastien Nocera <hadess@hadess.net>
2014-05-16 16:39:26 +0200

    housekeeping: Create test program for bug 730223
    $ mkdir /tmp/gsd-purge-temp-test
    $ mkdir /tmp/important/
    $ touch /tmp/important/dont-delete.txt
    $ ln -s /tmp/important/ /tmp/gsd-purge-temp-test/
    $ ls -l /tmp/gsd-purge-temp-test/
    0 lrwxrwxrwx. 1 hadess hadess 15 May 16 16:41 important -> /tmp/important/
    $ ./gsd-purge-temp-test
    <ctrl+c>
    $ ls -l /tmp/gsd-purge-temp-test/
    total 0
    $ ls -l /tmp/important/
    total 0

---
+++ plugins/housekeeping/gsd-purge-temp-test.c
@@ -0,0 +1,60 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ * vim: set et sw=8 ts=8:
+ *
+ * Copyright (c) 2008, Novell, Inc.
+ *
+ * Authors: Vincent Untz <vuntz@gnome.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "config.h"
+#include <gtk/gtk.h>
+#include <libnotify/notify.h>
+#include "gsd-disk-space.h"
+
+int
+main (int    argc,
+      char **argv)
+{
+        GFile *file;
+        DeleteData *data;
+        GDateTime *old;
+        GMainLoop *loop;
+
+        g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
+
+        gtk_init (&argc, &argv);
+        notify_init ("gsd-purge-temp-test");
+        loop = g_main_loop_new (NULL, FALSE);
+
+        file = g_file_new_for_path ("/tmp/gsd-purge-temp-test");
+        if (!g_file_query_exists (file, NULL)) {
+                g_warning ("Create /tmp/gsd-purge-temp-test and add some files to it to test deletion by date");
+                g_object_unref (file);
+                return 1;
+        }
+
+        old = g_date_time_new_now_local ();
+        data = delete_data_new (file, NULL, old, FALSE, FALSE, 0);
+        delete_recursively_by_age (data);
+        delete_data_unref (data);
+        g_object_unref (file);
+
+        g_main_loop_run (loop);
+
+        return 0;
+}
+
