29a4148c3c1db7d8ed68f2d6465188d469bbb0e8
Rui Matos <tiagomatos@gmail.com>
2014-10-17 17:07:08 +0200

    clipboard: Call stop from finalize

--- plugins/clipboard/gsd-clipboard-manager.c
+++ plugins/clipboard/gsd-clipboard-manager.c
@@ -1011,6 +1011,8 @@ gsd_clipboard_manager_finalize (GObject *object)
 
         g_return_if_fail (clipboard_manager->priv != NULL);
 
+        gsd_clipboard_manager_stop (clipboard_manager);
+
         if (clipboard_manager->priv->start_idle_id !=0)
                 g_source_remove (clipboard_manager->priv->start_idle_id);
 
