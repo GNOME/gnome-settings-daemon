b500520d2e09ea50e2898fb9a9bab28d2aa4cae3
Rui Matos <tiagomatos@gmail.com>
2014-11-14 23:24:23 +0100

    xrandr: Free GnomeRRConfigs on stop
    They won't leak if the plugin is stopped and started again but they do
    if we reach finalize() which isn't really a problem but there's no
    reason to keep them around anyway.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=740151

--- plugins/xrandr/gsd-xrandr-manager.c
+++ plugins/xrandr/gsd-xrandr-manager.c
@@ -893,14 +893,8 @@ sanitize (GsdXrandrManager *manager, GPtrArray *array)
 }
 
 static void
-generate_fn_f7_configs (GsdXrandrManager *mgr)
+free_fn_f7_configs (GsdXrandrManager *mgr)
 {
-        GPtrArray *array = g_ptr_array_new ();
-        GnomeRRScreen *screen = mgr->priv->rw_screen;
-
-        g_debug ("Generating configurations");
-
-        /* Free any existing list of configurations */
         if (mgr->priv->fn_f7_configs) {
                 int i;
 
@@ -911,6 +905,18 @@ generate_fn_f7_configs (GsdXrandrManager *mgr)
                 mgr->priv->fn_f7_configs = NULL;
                 mgr->priv->current_fn_f7_config = -1;
         }
+}
+
+static void
+generate_fn_f7_configs (GsdXrandrManager *mgr)
+{
+        GPtrArray *array = g_ptr_array_new ();
+        GnomeRRScreen *screen = mgr->priv->rw_screen;
+
+        g_debug ("Generating configurations");
+
+        /* Free any existing list of configurations */
+        free_fn_f7_configs (mgr);
 
         g_ptr_array_add (array, gnome_rr_config_new_current (screen, NULL));
         g_ptr_array_add (array, make_clone_setup (mgr, screen));
@@ -1294,6 +1300,8 @@ gsd_xrandr_manager_stop (GsdXrandrManager *manager)
                                              manager->priv->device_removed_id);
                 manager->priv->device_manager = NULL;
         }
+
+        free_fn_f7_configs (manager);
 
         log_open ();
         log_msg ("STOPPING XRANDR PLUGIN\n------------------------------------------------------------\n");
