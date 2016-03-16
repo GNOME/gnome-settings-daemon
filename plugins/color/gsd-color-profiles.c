GNOME/gnome-settings-daemon

commit c41c90ae014b61a7dce12ee1886035e95e997725

color: Fix a crash when cancelling loading profiles
https://bugzilla.gnome.org/show_bug.cgi?id=763382

plugins/color/gsd-color-profiles.c
@@ -57,11 +57,11 @@ gcm_session_client_connect_cb (GObject *source_object,
 {
         gboolean ret;
         GError *error = NULL;
-        GsdColorProfiles *profiles = GSD_COLOR_PROFILES (user_data);
-        GsdColorProfilesPrivate *priv = profiles->priv;
+        CdClient *client = CD_CLIENT (source_object);
+        GsdColorProfiles *profiles;
 
         /* connected */
-        ret = cd_client_connect_finish (profiles->priv->client, res, &error);
+        ret = cd_client_connect_finish (client, res, &error);
         if (!ret) {
                 if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
                         g_warning ("failed to connect to colord: %s", error->message);
@@ -70,6 +70,7 @@ gcm_session_client_connect_cb (GObject *source_object,
         }
 
         /* is there an available colord instance? */
+        profiles = GSD_COLOR_PROFILES (user_data);
         ret = cd_client_get_has_server (profiles->priv->client);
         if (!ret) {
                 g_warning ("There is no colord server available");

commit c07fa9c4a057f60290c859319f927401c4c277ed

color: Fix compile failure introduced with c41c90ae

plugins/color/gsd-color-profiles.c
@@ -78,7 +78,7 @@ gcm_session_client_connect_cb (GObject *source_object,
         }
 
         /* add profiles */
-        ret = cd_icc_store_search_kind (priv->icc_store,
+        ret = cd_icc_store_search_kind (profiles->priv->icc_store,
                                         CD_ICC_STORE_SEARCH_KIND_USER,
                                         CD_ICC_STORE_SEARCH_FLAGS_CREATE_LOCATION,
                                         profiles->priv->cancellable,