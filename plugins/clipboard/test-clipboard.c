32b8dd042197d4c6c28844dc45d7cb57c85652b9
Bastien Nocera <hadess@hadess.net>
2016-09-25 21:43:41 +0200

    clipboard: Add test application
    https://bugzilla.gnome.org/show_bug.cgi?id=741688

---
+++ plugins/clipboard/test-clipboard.c
@@ -0,0 +1,7 @@
+#define NEW gsd_clipboard_manager_new
+#define START gsd_clipboard_manager_start
+#define STOP gsd_clipboard_manager_stop
+#define MANAGER GsdClipboardManager
+#include "gsd-clipboard-manager.h"
+
+#include "test-plugin.h"
