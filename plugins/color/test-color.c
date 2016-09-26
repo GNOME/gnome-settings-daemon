e29a17f5316b0176d3d2ee1fcfeda0dd8ec12d01
Bastien Nocera <hadess@hadess.net>
2016-09-26 12:08:54 +0200

    color: Add test application
    https://bugzilla.gnome.org/show_bug.cgi?id=741688

---
+++ plugins/color/test-color.c
@@ -0,0 +1,7 @@
+#define NEW gsd_color_manager_new
+#define START gsd_color_manager_start
+#define STOP gsd_color_manager_stop
+#define MANAGER GsdColorManager
+#include "gsd-color-manager.h"
+
+#include "test-plugin.h"
