70e90f817bff29eedd3d649460048404cfcf66f1
Bastien Nocera <hadess@hadess.net>
2016-10-05 14:26:09 +0200

    dummy: Add missing test-dummy.c source
    https://bugzilla.gnome.org/show_bug.cgi?id=741688#c67

---
+++ plugins/dummy/test-dummy.c
@@ -0,0 +1,7 @@
+#define NEW gsd_dummy_manager_new
+#define START gsd_dummy_manager_start
+#define STOP gsd_dummy_manager_stop
+#define MANAGER GsdDummyManager
+#include "gsd-dummy-manager.h"
+
+#include "test-plugin.h"
