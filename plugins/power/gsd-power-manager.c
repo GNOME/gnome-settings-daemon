GNOME/gnome-settings-daemon

commit 867b20398e4b9c04833e618603dfd90297de82ad

power: "0.0" is not a valid light level
This is the equivalent of "undefined" for the accelerometer
orientation. Don't use it to prime our nice ambient light
readings curve.

https://bugzilla.gnome.org/show_bug.cgi?id=786164

plugins/power/gsd-power-manager.c
@@ -2550,7 +2550,7 @@ iio_proxy_changed (GsdPowerManager *manager)
         if (val_has == NULL || !g_variant_get_boolean (val_has))
                 goto out;
         val_als = g_dbus_proxy_get_cached_property (manager->priv->iio_proxy, "LightLevel");
-        if (val_als == NULL)
+        if (val_als == NULL || g_variant_get_double (val_als) == 0.0) {
                 goto out;
         manager->priv->ambient_last_absolute = g_variant_get_double (val_als);
         g_debug ("Read last absolute light level: %f", manager->priv->ambient_last_absolute);

commit 8a98aea8f3a00c8f9e5939192f7cf9952e60d759

Build fixes for the power plugin
Commit 476154fe and commit 867b2039 broke the build.

Full Continuous log:

http://build.gnome.org/continuous/buildmaster/builds/2017/08/21/49/build/log-gnome-settings-daemon.txt

https://bugzilla.gnome.org/show_bug.cgi?id=786577

plugins/power/gsd-power-manager.c
@@ -993,7 +993,7 @@ iio_proxy_claim_light (GsdPowerManager *manager, gboolean active)
                 return;
         if (!manager->priv->backlight_available)
                 return;
-	if (active && !manager->priv->session_is_active) {
+	if (active && !manager->priv->session_is_active)
 		return;
 
         if (!g_dbus_proxy_call_sync (manager->priv->iio_proxy,
@@ -2550,7 +2550,7 @@ iio_proxy_changed (GsdPowerManager *manager)
         if (val_has == NULL || !g_variant_get_boolean (val_has))
                 goto out;
         val_als = g_dbus_proxy_get_cached_property (manager->priv->iio_proxy, "LightLevel");
-        if (val_als == NULL || g_variant_get_double (val_als) == 0.0) {
+        if (val_als == NULL || g_variant_get_double (val_als) == 0.0)
                 goto out;
         manager->priv->ambient_last_absolute = g_variant_get_double (val_als);
         g_debug ("Read last absolute light level: %f", manager->priv->ambient_last_absolute);