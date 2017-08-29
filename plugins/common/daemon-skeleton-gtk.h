9fddecc4641709308dec0300a778a32a2b17aafd
Sebastien Bacher <seb128@ubuntu.com>
2017-08-29 16:16:46 +0200

    common: call setlocale
    Without that the notifications are displayed untranslated
    https://bugzilla.gnome.org/show_bug.cgi?id=786978

--- plugins/common/daemon-skeleton-gtk.h
+++ plugins/common/daemon-skeleton-gtk.h
@@ -193,6 +193,7 @@ main (int argc, char **argv)
         bindtextdomain (GETTEXT_PACKAGE, GNOME_SETTINGS_LOCALEDIR);
         bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
         textdomain (GETTEXT_PACKAGE);
+        setlocale (LC_ALL, "");
 
         set_empty_gtk_theme (TRUE);
 
--- plugins/common/daemon-skeleton.h
+++ plugins/common/daemon-skeleton.h
@@ -171,6 +171,7 @@ main (int argc, char **argv)
         bindtextdomain (GETTEXT_PACKAGE, GNOME_SETTINGS_LOCALEDIR);
         bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
         textdomain (GETTEXT_PACKAGE);
+        setlocale (LC_ALL, "");
 
         context = g_option_context_new (NULL);
         g_option_context_add_main_entries (context, entries, GETTEXT_PACKAGE);
