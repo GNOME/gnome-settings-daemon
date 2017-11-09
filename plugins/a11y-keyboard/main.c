9553071d0c26c3150a5f55bd93ebb4673922836a
Olivier Fourdan <ofourdan@redhat.com>
2017-11-09 15:55:40 +0100

    plugins: Remove a11y-keyboard
    This feature is handled by mutter/clutter backends.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=790121

--- plugins/a11y-keyboard/main.c
+++
@@ -1,7 +0,0 @@
-#define NEW gsd_a11y_keyboard_manager_new
-#define START gsd_a11y_keyboard_manager_start
-#define STOP gsd_a11y_keyboard_manager_stop
-#define MANAGER GsdA11yKeyboardManager
-#include "gsd-a11y-keyboard-manager.h"
-
-#include "daemon-skeleton-gtk.h"
