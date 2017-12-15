65253003cd54b066e1d0c8c89a4b7ee6225555f1
Iain Lane <iain@orangesquash.org.uk>
2017-12-15 15:03:55 +0000

    common: Have plugins claim a name on the bus when they are ready
    The plugins are about to switch to being activated by systemd. We need a
    way for them to signal to systemd when they are ready. We'd like to
    avoid linking to libsystemd, so sd_notify() is out - let's have the
    plugins claim a name on the bus and then we can create Type=dbus units.

--- plugins/common/daemon-skeleton.h
+++ plugins/common/daemon-skeleton.h
@@ -24,6 +24,10 @@
 #error Include PLUGIN_CFLAGS in the daemon s CFLAGS
 #endif /* !PLUGIN_NAME */
 
+#ifndef PLUGIN_DBUS_NAME
+#error Include PLUGIN_DBUS_NAME in the daemon s CFLAGS
+#endif /* !PLUGIN_DBUS_NAME */
+
 #define GNOME_SESSION_DBUS_NAME                     "org.gnome.SessionManager"
 #define GNOME_SESSION_CLIENT_PRIVATE_DBUS_INTERFACE "org.gnome.SessionManager.ClientPrivate"
 
@@ -192,12 +196,37 @@ install_signal_handler (GMainLoop *loop)
   g_source_attach (source, NULL);
 }
 
+static void
+bus_acquired_cb (GDBusConnection *connection,
+                 const gchar *name,
+                 gpointer user_data G_GNUC_UNUSED)
+{
+        g_debug ("%s: acquired bus %p for name %s", G_STRFUNC, connection, name);
+}
+
+static void
+name_acquired_cb (GDBusConnection *connection,
+                  const gchar *name,
+                  gpointer user_data G_GNUC_UNUSED)
+{
+        g_debug ("%s: acquired name %s on bus %p", G_STRFUNC, name, connection);
+}
+
+static void
+name_lost_cb (GDBusConnection *connection,
+              const gchar *name,
+              gpointer user_data G_GNUC_UNUSED)
+{
+        g_debug ("%s: lost name %s on bus %p", G_STRFUNC, name, connection);
+}
+
 int
 main (int argc, char **argv)
 {
         GError *error = NULL;
         GOptionContext *context;
         GMainLoop *loop;
+        guint name_own_id;
 
         bindtextdomain (GETTEXT_PACKAGE, GNOME_SETTINGS_LOCALEDIR);
         bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
@@ -248,11 +277,22 @@ main (int argc, char **argv)
 		}
 	}
 
+	name_own_id = g_bus_own_name (G_BUS_TYPE_SESSION,
+				      PLUGIN_DBUS_NAME,
+				      G_BUS_NAME_OWNER_FLAGS_DO_NOT_QUEUE,
+				      bus_acquired_cb,
+				      name_acquired_cb,
+				      name_lost_cb,
+				      NULL, /* user_data */
+				      NULL /* user_data_free_func */);
+
         g_main_loop_run (loop);
 
 	if (should_run ())
 		STOP (manager);
+
         g_object_unref (manager);
+        g_bus_unown_name (name_own_id);
 
         return 0;
 }
