d3f73ece9410ae91e66f26653395647cb61784ac
Guido Günther <agx@sigxcpu.org>
2018-08-29 10:06:40 +0200

    Initial wwan plugin
    Heavily based on code from nm-applet.
    
    Follow-Ups:
       - Allow to store SIM in keyring
       - Handle PUKs? (or do that in g-c-c)

---
+++ plugins/wwan/main.c
@@ -0,0 +1,7 @@
+#define NEW gsd_wwan_manager_new
+#define START gsd_wwan_manager_start
+#define STOP gsd_wwan_manager_stop
+#define MANAGER GsdWwanManager
+#include "gsd-wwan-manager.h"
+
+#include "daemon-skeleton.h"
