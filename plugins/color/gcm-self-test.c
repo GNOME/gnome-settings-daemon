b4d3f0ca863272172fcbb5295578dd32ae634840
Benjamin Berg <bberg@redhat.com>
2019-01-03 12:52:00 +0100

    color: Remove vendor name check from EDID test
    The vendor name is resolved using the systemd hwdb. There is no need to
    test the hwdb lookup here. Not doing the check means that the test will
    run even when systemd changes or if the lookup fails for some other
    reason.
    
    Thanks to Andreas Henriksson <andreas@fatal.se> for finding this and
    proposing a fix.
    
    Fixes: #126

--- plugins/color/gcm-self-test.c
+++ plugins/color/gcm-self-test.c
@@ -240,10 +240,11 @@ gcm_test_edid_func (void)
         g_assert (ret);
 
         g_assert_cmpstr (gcm_edid_get_monitor_name (edid), ==, "L225W");
-        g_assert_cmpstr (gcm_edid_get_vendor_name (edid), ==, "Goldstar Company Ltd");
         g_assert_cmpstr (gcm_edid_get_serial_number (edid), ==, "34398");
         g_assert_cmpstr (gcm_edid_get_eisa_id (edid), ==, NULL);
         g_assert_cmpstr (gcm_edid_get_checksum (edid), ==, "0bb44865bb29984a4bae620656c31368");
+        /* Vendor name is not tested as it is resolved from the PNP ID and may
+         * change when systemd updates the hwdb. */
         g_assert_cmpstr (gcm_edid_get_pnp_id (edid), ==, "GSM");
         g_assert_cmpint (gcm_edid_get_height (edid), ==, 30);
         g_assert_cmpint (gcm_edid_get_width (edid), ==, 47);
@@ -261,7 +262,6 @@ gcm_test_edid_func (void)
         g_assert (ret);
 
         g_assert_cmpstr (gcm_edid_get_monitor_name (edid), ==, NULL);
-        g_assert_cmpstr (gcm_edid_get_vendor_name (edid), ==, "IBM Brasil");
         g_assert_cmpstr (gcm_edid_get_serial_number (edid), ==, NULL);
         g_assert_cmpstr (gcm_edid_get_eisa_id (edid), ==, "LTN154P2-L05");
         g_assert_cmpstr (gcm_edid_get_checksum (edid), ==, "e1865128c7cd5e5ed49ecfc8102f6f9c");
