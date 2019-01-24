ea2701ce346eb27982c5dadbdaddc509b4cd9a0d
Benjamin Berg <bberg@redhat.com>
2019-01-24 15:43:48 +0100

    power: Build and test sysfs backlight support only on Linux
    Configuring the backlight using sysfs is only supported on linux. We
    depend on GUdev on linux, so that we can just check for the platform.
    
    This avoids enabling the sysfs backlight helper on platforms that have
    GUdev but do not support backlight control using sysfs.

--- plugins/power/gsd-backlight.c
+++ plugins/power/gsd-backlight.c
@@ -25,9 +25,9 @@
 #include "gsd-power-constants.h"
 #include "gsd-power-manager.h"
 
-#ifdef HAVE_GUDEV
+#ifdef __linux__
 #include <gudev/gudev.h>
-#endif /* HAVE_GUDEV */
+#endif /* __linux__ */
 
 struct _GsdBacklight
 {
@@ -39,7 +39,7 @@ struct _GsdBacklight
         gint brightness_target;
         gint brightness_step;
 
-#ifdef HAVE_GUDEV
+#ifdef __linux__
         GUdevClient *udev;
         GUdevDevice *udev_device;
 
@@ -47,7 +47,7 @@ struct _GsdBacklight
         GQueue tasks;
 
         gint idle_update;
-#endif
+#endif /* __linux__ */
 
         GnomeRRScreen *rr_screen;
 };
@@ -70,7 +70,7 @@ G_DEFINE_TYPE_EXTENDED (GsdBacklight, gsd_backlight, G_TYPE_OBJECT, 0,
                         G_IMPLEMENT_INTERFACE (G_TYPE_INITABLE,
                                                gsd_backlight_initable_iface_init);)
 
-#ifdef HAVE_GUDEV
+#ifdef __linux__
 static GUdevDevice*
 gsd_backlight_udev_get_type (GList *devices, const gchar *type)
 {
@@ -395,7 +395,7 @@ gsd_backlight_process_taskqueue (GsdBacklight *backlight)
         /* And run it! */
         gsd_backlight_run_set_helper (backlight, to_run);
 }
-#endif /* HAVE_GUDEV */
+#endif /* __linux__ */
 
 static GnomeRROutput*
 gsd_backlight_rr_find_output (GsdBacklight *backlight, gboolean controllable)
@@ -464,7 +464,7 @@ gsd_backlight_set_brightness_val_async (GsdBacklight *backlight,
 
         task = g_task_new (backlight, cancellable, callback, user_data);
 
-#ifdef HAVE_GUDEV
+#ifdef __linux__
         if (backlight->udev_device != NULL) {
                 BacklightHelperData *task_data;
 
@@ -478,7 +478,7 @@ gsd_backlight_set_brightness_val_async (GsdBacklight *backlight,
 
                 return;
         }
-#endif /* HAVE_GUDEV */
+#endif /* __linux__ */
 
         /* Fallback to setting via GNOME RR/X11 */
         output = gsd_backlight_rr_find_output (backlight, TRUE);
@@ -716,11 +716,11 @@ gsd_backlight_initable_init (GInitable       *initable,
                 return FALSE;
         }
 
-#ifdef HAVE_GUDEV
+#ifdef __linux__
         /* Try finding a udev device. */
         if (gsd_backlight_udev_init (backlight))
                 goto found;
-#endif /* HAVE_GUDEV */
+#endif /* __linux__ */
 
         /* Try GNOME RR as a fallback. */
         output = gsd_backlight_rr_find_output (backlight, TRUE);
@@ -755,7 +755,7 @@ gsd_backlight_finalize (GObject *object)
 {
         GsdBacklight *backlight = GSD_BACKLIGHT (object);
 
-#ifdef HAVE_GUDEV
+#ifdef __linux__
         g_assert (backlight->active_task == NULL);
         g_assert (g_queue_is_empty (&backlight->tasks));
         g_clear_object (&backlight->udev);
@@ -764,7 +764,7 @@ gsd_backlight_finalize (GObject *object)
                 g_source_remove (backlight->idle_update);
                 backlight->idle_update = 0;
         }
-#endif /* HAVE_GUDEV */
+#endif /* __linux__ */
 
         g_clear_object (&backlight->rr_screen);
 }
@@ -807,10 +807,10 @@ gsd_backlight_init (GsdBacklight *backlight)
         backlight->brightness_val = -1;
         backlight->brightness_step = 1;
 
-#ifdef HAVE_GUDEV
+#ifdef __linux__
         backlight->active_task = NULL;
         g_queue_init (&backlight->tasks);
-#endif /* HAVE_GUDEV */
+#endif /* __linux__ */
 }
 
 GsdBacklight *
