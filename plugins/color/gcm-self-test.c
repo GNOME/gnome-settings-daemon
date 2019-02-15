a101e96b493751f7ca40ec8ee3d7e862030e2d5a
Benjamin Berg <bberg@redhat.com>
2019-02-15 10:52:39 +0100

    color: Allow night light to be always on
    Currently if the user configures the "from" and "to" times for the night
    light schedule to be close together or equal, g-s-d gets confused as it
    still tries to smear the transition over a full hour period.
    
    To fix this, first make sure that the smearing period is short enough to
    fit between sunrise and sunset. Also redefine equal start/end times when
    calculating whether a time falls into a range as the full 24h period.
    
    This effectively means that we will smear over the full day or night
    time when the sunrise and sunset are closer than an hour to each other.
    It also means that we always enable night light when the times are
    equal.
    
    Based on a patch by Matthew Leeds <matthew.leeds@endlessm.com>.

--- plugins/color/gcm-self-test.c
+++ plugins/color/gcm-self-test.c
@@ -338,17 +338,21 @@ gcm_test_frac_day (void)
         g_assert_cmpfloat (fd, <, fd_actual + 0.01);
 
         /* test same day */
-        g_assert (gsd_night_light_frac_day_is_between (12, 6, 20));
-        g_assert (!gsd_night_light_frac_day_is_between (5, 6, 20));
-        g_assert (gsd_night_light_frac_day_is_between (12, 0, 24));
-        g_assert (gsd_night_light_frac_day_is_between (12, -1, 25));
+        g_assert_true (gsd_night_light_frac_day_is_between (12, 6, 20));
+        g_assert_false (gsd_night_light_frac_day_is_between (5, 6, 20));
+        g_assert_true (gsd_night_light_frac_day_is_between (12, 0, 24));
+        g_assert_true (gsd_night_light_frac_day_is_between (12, -1, 25));
 
         /* test rollover to next day */
-        g_assert (gsd_night_light_frac_day_is_between (23, 20, 6));
-        g_assert (!gsd_night_light_frac_day_is_between (12, 20, 6));
+        g_assert_true (gsd_night_light_frac_day_is_between (23, 20, 6));
+        g_assert_false (gsd_night_light_frac_day_is_between (12, 20, 6));
 
         /* test rollover to the previous day */
-        g_assert (gsd_night_light_frac_day_is_between (5, 16, 8));
+        g_assert_true (gsd_night_light_frac_day_is_between (5, 16, 8));
+
+        /* test equality */
+        g_assert_true (gsd_night_light_frac_day_is_between (12, 0.5, 24.5));
+        g_assert_true (gsd_night_light_frac_day_is_between (0.5, 0.5, 0.5));
 }
 
 int
