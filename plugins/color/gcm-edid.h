391498151c01488365a9453be41805a48ad1cbec
Benjamin Berg <bberg@redhat.com>
2019-02-20 14:54:42 +0100

    color: Use G_DECLARE_FINAL_TYPE for GcmEdid

--- plugins/color/gcm-edid.h
+++ plugins/color/gcm-edid.h
@@ -28,34 +28,14 @@
 G_BEGIN_DECLS
 
 #define GCM_TYPE_EDID           (gcm_edid_get_type ())
-#define GCM_EDID(o)             (G_TYPE_CHECK_INSTANCE_CAST ((o), GCM_TYPE_EDID, GcmEdid))
-#define GCM_EDID_CLASS(k)       (G_TYPE_CHECK_CLASS_CAST((k), GCM_TYPE_EDID, GcmEdidClass))
-#define GCM_IS_EDID(o)          (G_TYPE_CHECK_INSTANCE_TYPE ((o), GCM_TYPE_EDID))
-#define GCM_IS_EDID_CLASS(k)    (G_TYPE_CHECK_CLASS_TYPE ((k), GCM_TYPE_EDID))
-#define GCM_EDID_GET_CLASS(o)   (G_TYPE_INSTANCE_GET_CLASS ((o), GCM_TYPE_EDID, GcmEdidClass))
+G_DECLARE_FINAL_TYPE (GcmEdid, gcm_edid, GCM, EDID, GObject)
+
 #define GCM_EDID_ERROR          (gcm_edid_error_quark ())
-
-typedef struct _GcmEdidPrivate  GcmEdidPrivate;
-typedef struct _GcmEdid         GcmEdid;
-typedef struct _GcmEdidClass    GcmEdidClass;
-
-struct _GcmEdid
-{
-         GObject                 parent;
-         GcmEdidPrivate         *priv;
-};
-
-struct _GcmEdidClass
-{
-        GObjectClass    parent_class;
-};
-
 enum
 {
         GCM_EDID_ERROR_FAILED_TO_PARSE
 };
 
-GType            gcm_edid_get_type                      (void);
 GQuark           gcm_edid_error_quark                   (void);
 GcmEdid         *gcm_edid_new                           (void);
 void             gcm_edid_reset                         (GcmEdid                *edid);
