2cfea9832a73ab202e21199852a37a170e488870
Carlos Garnacho <carlosg@gnome.org>
2019-03-05 14:10:49 +0100

    clibpoard: Drop clipboard manager
    This responsibility was taken over by mutter, where it can be made to work
    both on Wayland and X11. This clipboard manager implementation becomes
    superfluous then.

--- plugins/clipboard/main.c
+++
@@ -1,8 +0,0 @@
-#define NEW gsd_clipboard_manager_new
-#define START gsd_clipboard_manager_start
-#define STOP gsd_clipboard_manager_stop
-#define MANAGER GsdClipboardManager
-#define GDK_BACKEND "x11"
-#include "gsd-clipboard-manager.h"
-
-#include "daemon-skeleton-gtk.h"
