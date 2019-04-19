b7a63086dad70e02f5a0fbda96deacaba06b0cdf
Benjamin Berg <bberg@redhat.com>
2019-04-20 00:39:15 +0200

    plugins: Add systemd user service files for all the plugins
    Using the facility added in the previous commit, we can add systemd user
    service files for all plugins and know when they have started up.
    
    This is roughly based on the work previously done by Iain Lane
    <iain@orangesquash.org.uk> and Bastien Nocera hadess@hadess.net>.
    
    For each g-s-d process we have a service and a target file. This
    separation only exists to contain dependency failures which would cause
    an OnFailure action to trigger and is needed so that we can use
    OnFailure for the gnome-session fail-whale
    (gnome-session-failed.target).
    
    In general, the approach taken is that we start g-s-d processes after
    gnome-session-initialized.target and before gnome-session.target.
    
    We want to be able to selectively start the services only when one or
    more dependencies are there, or even mask out services under some
    conditions. The approach taken is the following:
     * To mask a service, use a Conflicts entry. This is e.g. used to not
       start certain services in GDM using
         Conflicts=gnome-session@gnome-login.target
     * To depend on multiple targets to be up and running to start, we set
       each of these targets in Requisite/After/PartOf/WantedBy. We always
       do this for nome-session-initialized.target but this method is
       extensible to any number of further targets (e.g. bluetooth.target)

--- plugins/common/daemon-skeleton.h
+++ plugins/common/daemon-skeleton.h
@@ -43,36 +43,6 @@ static GOptionEntry entries[] = {
         {NULL}
 };
 
-static const char *gdm_helpers[] = {
-	"a11y-keyboard",
-	"a11y-settings",
-	"clipboard",
-	"color",
-	"keyboard",
-	"media-keys",
-	"power",
-	"smartcard",
-	"sound",
-	"xsettings"
-};
-
-static gboolean
-should_run (void)
-{
-	const char *session_mode;
-	guint i;
-
-	session_mode = g_getenv ("GNOME_SHELL_SESSION_MODE");
-	if (g_strcmp0 (session_mode, "gdm") != 0)
-		return TRUE;
-
-	for (i = 0; i < G_N_ELEMENTS (gdm_helpers); i++) {
-		if (g_str_equal (PLUGIN_NAME, gdm_helpers[i]))
-			return TRUE;
-	}
-	return FALSE;
-}
-
 static void
 respond_to_end_session (GDBusProxy *proxy)
 {
@@ -268,14 +238,11 @@ main (int argc, char **argv)
         manager = NEW ();
 	register_with_gnome_session (loop);
 
-	if (should_run ()) {
-		error = NULL;
-		if (!START (manager, &error)) {
-			fprintf (stderr, "Failed to start: %s\n", error->message);
-			g_error_free (error);
-			exit (1);
-		}
-	}
+        if (!START (manager, &error)) {
+                fprintf (stderr, "Failed to start: %s\n", error->message);
+                g_error_free (error);
+                exit (1);
+        }
 
 	name_own_id = g_bus_own_name (G_BUS_TYPE_SESSION,
 				      PLUGIN_DBUS_NAME,
@@ -288,8 +255,7 @@ main (int argc, char **argv)
 
         g_main_loop_run (loop);
 
-	if (should_run ())
-		STOP (manager);
+        STOP (manager);
 
         g_object_unref (manager);
         g_bus_unown_name (name_own_id);
