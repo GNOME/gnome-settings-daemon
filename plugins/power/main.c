9dedec9c6f283995912d2721c6551ee671eb6d8f
Carlos Garnacho <carlosg@gnome.org>
2019-05-23 21:11:54 +0200

    power: Do not require X11 GDK backend

--- plugins/power/main.c
+++ plugins/power/main.c
@@ -2,7 +2,6 @@
 #define START gsd_power_manager_start
 #define STOP gsd_power_manager_stop
 #define MANAGER GsdPowerManager
-#define GDK_BACKEND "x11"
 #include "gsd-power-manager.h"
 
 #include "daemon-skeleton-gtk.h"
