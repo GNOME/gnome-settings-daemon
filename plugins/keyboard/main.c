3ac1dae1f1267e4736f1ef6de02e5eceb96a7378
Carlos Garnacho <carlosg@gnome.org>
2019-05-23 21:11:41 +0200

    keyboard: Do not require X11 GDK backend

--- plugins/keyboard/main.c
+++ plugins/keyboard/main.c
@@ -2,7 +2,6 @@
 #define START gsd_keyboard_manager_start
 #define STOP gsd_keyboard_manager_stop
 #define MANAGER GsdKeyboardManager
-#define GDK_BACKEND "x11"
 #include "gsd-keyboard-manager.h"
 
 #include "daemon-skeleton-gtk.h"
