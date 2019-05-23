808363448155cd06dfe2211ec91da9b862321f66
Carlos Garnacho <carlosg@gnome.org>
2019-05-23 21:10:43 +0200

    color: Do not require X11 GDK backend

--- plugins/color/main.c
+++ plugins/color/main.c
@@ -2,7 +2,6 @@
 #define START gsd_color_manager_start
 #define STOP gsd_color_manager_stop
 #define MANAGER GsdColorManager
-#define GDK_BACKEND "x11"
 #include "gsd-color-manager.h"
 
 #include "daemon-skeleton-gtk.h"
