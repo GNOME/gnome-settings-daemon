d5600c337e9d2bf07775a0460311986d29ea9584
Olivier Fourdan <ofourdan@redhat.com>
2019-06-06 15:18:09 +0200

    mouse: Remove mouse plugin
    There is nothing left in the mouse plugin but the settings migration,
    which occurred more than 3 years ago in GNOME 3.14.
    
    Remove that plugin altogether.
    
    Note, we keep the schemas as this is still used by the XSettings plugin.

--- plugins/mouse/main.c
+++
@@ -1,7 +0,0 @@
-#define NEW gsd_mouse_manager_new
-#define START gsd_mouse_manager_start
-#define STOP gsd_mouse_manager_stop
-#define MANAGER GsdMouseManager
-#include "gsd-mouse-manager.h"
-
-#include "daemon-skeleton.h"
