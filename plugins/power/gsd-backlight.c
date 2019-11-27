dd2395001ebbce6f8570713482687104b248d317
Bastien Nocera <hadess@hadess.net>
2019-11-27 12:57:13 +0100

    power: Don't throw warnings when systemd is too old
    We still have a fallback mechanism, so don't be noisy if systemd is too
    old.

--- plugins/power/gsd-backlight.c
+++ plugins/power/gsd-backlight.c
@@ -848,6 +848,11 @@ gsd_backlight_initable_init (GInitable       *initable,
                          * this is expected.
                          */
                         g_clear_error (&logind_error);
+                } else if (g_error_matches (logind_error, G_DBUS_ERROR,
+                                            G_DBUS_ERROR_UNKNOWN_METHOD)) {
+                        /* systemd version is too old, so ignore.
+                         */
+                        g_clear_error (&logind_error);
                 } else {
                         /* Fail on anything else */
                         g_clear_object (&backlight->logind_proxy);
