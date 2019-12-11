179a84dc50b0def7397c930935088f64a491b21e
Benjamin Berg <bberg@redhat.com>
2019-12-12 00:41:31 +0100

    dummy: Remove gsd-dummy binary
    The binary was only needed for the stub gsd daemons that may not be
    installed. The generated autostart files are not referencing gsd-dummy
    anymore. As such, we can remove the binary completely.

--- plugins/dummy/main.c
+++
@@ -1,7 +0,0 @@
-#define NEW gsd_dummy_manager_new
-#define START gsd_dummy_manager_start
-#define STOP gsd_dummy_manager_stop
-#define MANAGER GsdDummyManager
-#include "gsd-dummy-manager.h"
-
-#include "daemon-skeleton.h"
