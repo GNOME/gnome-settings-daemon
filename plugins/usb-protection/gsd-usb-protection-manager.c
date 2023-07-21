64dd8dbfca25f87f1ab5f2dfca9702d74e248d04
Tobias Mueller <muelli@cryptobitch.de>
2023-07-21 10:34:05 +0200

    usb protection: Re-introduce the string change for the device activation
    This reverts commit c42110d85fff9fd700a6a7cc62b14e84efff0942.

--- plugins/usb-protection/gsd-usb-protection-manager.c
+++ plugins/usb-protection/gsd-usb-protection-manager.c
@@ -727,7 +727,9 @@ on_usbguard_signal (GDBusProxy *proxy,
                         } else {
                                 show_notification (manager,
                                                    _("USB device blocked"),
-                                                   _("The new inserted device has been blocked because the USB protection is active."));
+                                                   _("The new inserted device has been blocked because the USB protection is active. "
+                                                     "If you want to activate the device, disable the USB protection and re-plug "
+                                                     "the deivce."));
                         }
                 } else {
                     /* This is protection level == Lockscreen, so we allow everything when the session is unlocked.
