4fd98f8c91257999319b77da8c124ce7f6586f57
Jamie Murphy <hello@itsjamie.dev>
2024-06-19 13:07:36 -0700

    color-scheme: Add ColorSchemeManager
    Create a ColourSchemeManager and ColourScheme object that work to adjust
    the system colour scheme on timer.
    
    Fixes #788

---
+++ plugins/color-scheme/gsd-color-scheme-manager.h
@@ -0,0 +1,44 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2024 Jamie Murphy <jmurphy@gnome.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GSD_COLOR_SCHEME_MANAGER_H
+#define __GSD_COLOR_SCHEME_MANAGER_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define GSD_TYPE_COLOR_SCHEME_MANAGER  (gsd_color_scheme_manager_get_type ())
+#define GSD_COLOR_SCHEME_MANAGER_ERROR (gsd_color_scheme_manager_error_quark ())
+G_DECLARE_FINAL_TYPE (GsdColorSchemeManager, gsd_color_scheme_manager, GSD, COLOR_SCHEME_MANAGER, GObject)
+
+enum
+{
+    GSD_COLOR_SCHEME_MANAGER_FAILED
+};
+
+GQuark                  gsd_color_scheme_manager_error_quark  (void);
+
+GsdColorSchemeManager * gsd_color_scheme_manager_new          (void);
+gboolean                gsd_color_scheme_manager_start        (GsdColorSchemeManager  *manager,
+                                                               GError                **error);
+void                    gsd_color_scheme_manager_stop         (GsdColorSchemeManager  *manager);
+
+G_END_DECLS
+
+#endif /* __GSD_COLOR_SCHEME_MANAGER_H */
---
+++ plugins/color-scheme/main.c
@@ -0,0 +1,7 @@
+#define NEW gsd_color_scheme_manager_new
+#define START gsd_color_scheme_manager_start
+#define STOP gsd_color_scheme_manager_stop
+#define MANAGER GsdColorSchemeManager
+#include "gsd-color-scheme-manager.h"
+
+#include "daemon-skeleton-gtk.h"
