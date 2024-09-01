GNOME/gnome-settings-daemon

commit 7f8e33c77e71fcd195cc4fe766e2ca019840abfb

power: Stop using libgnome-rr
Instead use org.gnome.Mutter.DisplayConfig directly. The manual
processing of monitors and their state has been replaced with an
explicit 'HasExternalMonitor' property on
`org.gnome.Mutter.DisplayConfig`, and detecting and changing backlight
and the monitors have been replaced with an explicit `Backlight`
property and `SetBacklight` method on `org.gnome.Mutter.DisplayConfig`

plugins/power/gsd-backlight.c
@@ -19,7 +19,9 @@
 
 #include "config.h"
 #include <stdlib.h>
+#include <stdint.h>
 
+#include "gnome-settings-bus.h"
 #include "gsd-backlight.h"
 #include "gpm-common.h"
 #include "gsd-power-constants.h"
@@ -39,6 +41,9 @@ struct _GsdBacklight
         gint brightness_target;
         gint brightness_step;
 
+        uint32_t backlight_serial;
+        char *backlight_connector;
+
 #ifdef __linux__
         GDBusProxy *logind_proxy;
 
@@ -51,19 +56,21 @@ struct _GsdBacklight
         gint idle_update;
 #endif /* __linux__ */
 
-        GnomeRRScreen *rr_screen;
         gboolean builtin_display_disabled;
 };
 
 enum {
-        PROP_RR_SCREEN = 1,
-        PROP_BRIGHTNESS,
+        PROP_BRIGHTNESS = 1,
         PROP_LAST,
 };
 
 #define SYSTEMD_DBUS_NAME                       "org.freedesktop.login1"
 #define SYSTEMD_DBUS_PATH                       "/org/freedesktop/login1/session/auto"
 #define SYSTEMD_DBUS_INTERFACE                  "org.freedesktop.login1.Session"
+
+#define MUTTER_DBUS_NAME                       "org.gnome.Mutter.DisplayConfig"
+#define MUTTER_DBUS_PATH                       "/org/gnome/Mutter/DisplayConfig"
+#define MUTTER_DBUS_INTERFACE                  "org.gnome.Mutter.DisplayConfig"
 
 static GParamSpec *props[PROP_LAST];
 
@@ -400,30 +407,11 @@ gsd_backlight_process_taskqueue (GsdBacklight *backlight)
 }
 #endif /* __linux__ */
 
-static GnomeRROutput*
-gsd_backlight_rr_find_output (GsdBacklight *backlight, gboolean controllable)
+static const char *
+gsd_backlight_mutter_find_monitor (GsdBacklight *backlight,
+                                   gboolean      controllable)
 {
-        GnomeRROutput *output = NULL;
-        GnomeRROutput **outputs;
-        guint i;
-
-        /* search all X11 outputs for the device id */
-        outputs = gnome_rr_screen_list_outputs (backlight->rr_screen);
-        if (outputs == NULL)
-                goto out;
-
-        for (i = 0; outputs[i] != NULL; i++) {
-                gboolean builtin = gnome_rr_output_is_builtin_display (outputs[i]);
-                gint backlight = gnome_rr_output_get_backlight (outputs[i]);
-
-                g_debug("Output %d: %s, backlight %d", i, builtin ? "builtin" : "external", backlight);
-                if (builtin && (!controllable || backlight >= 0)) {
-                        output = outputs[i];
-                        break;
-                }
-        }
-out:
-        return output;
+        return backlight->backlight_connector;
 }
 
 /**
@@ -464,7 +452,7 @@ gsd_backlight_set_brightness_val_async (GsdBacklight *backlight,
 {
         GError *error = NULL;
         GTask *task = NULL;
-        GnomeRROutput *output;
+        const char *monitor;
         gint percent;
 
         value = MIN(backlight->brightness_max, value);
@@ -507,15 +495,24 @@ gsd_backlight_set_brightness_val_async (GsdBacklight *backlight,
         }
 #endif /* __linux__ */
 
-        /* Fallback to setting via GNOME RR/X11 */
-        output = gsd_backlight_rr_find_output (backlight, TRUE);
-        if (output) {
-                if (!gnome_rr_output_set_backlight (output, value, &error)) {
+        /* Fallback to setting via DisplayConfig (mutter) */
+        monitor = gsd_backlight_mutter_find_monitor (backlight, TRUE);
+        if (monitor) {
+                GsdDisplayConfig *display_config =
+                        gnome_settings_bus_get_display_config_proxy ();
+
+                if (!gsd_display_config_call_set_backlight_sync (display_config,
+                                                                 backlight->backlight_serial,
+                                                                 monitor,
+                                                                 value,
+                                                                 NULL,
+                                                                 &error)) {
                         g_task_return_error (task, error);
                         g_object_unref (task);
                         return;
                 }
-                backlight->brightness_val = gnome_rr_output_get_backlight (output);
+
+                backlight->brightness_val = value;
                 g_object_notify_by_pspec (G_OBJECT (backlight), props[PROP_BRIGHTNESS]);
                 g_task_return_int (task, gsd_backlight_get_brightness (backlight, NULL));
                 g_object_unref (task);
@@ -732,36 +729,7 @@ gsd_backlight_cycle_up_finish (GsdBacklight *backlight,
 const char*
 gsd_backlight_get_connector (GsdBacklight *backlight)
 {
-        GnomeRROutput *output;
-
-        output = gsd_backlight_rr_find_output (backlight, FALSE);
-        if (output == NULL)
-                return NULL;
-
-        return gnome_rr_output_get_name (output);
-}
-
-static void
-gsd_backlight_rr_screen_changed_cb (GnomeRRScreen *screen,
-                                    gpointer data)
-{
-        GsdBacklight *backlight = GSD_BACKLIGHT (data);
-        GnomeRROutput *output;
-        gboolean builtin_display_disabled = FALSE;
-
-        /* NOTE: Err on the side of assuming the backlight controlls something
-         *       even if we cannot find the output that belongs to it.
-         *       This might backfire on us obviously if the hardware claims it
-         *       can control a non-existing screen.
-         */
-        output = gsd_backlight_rr_find_output (backlight, FALSE);
-        if (output)
-                builtin_display_disabled = !gnome_rr_output_get_crtc (output);
-
-        if (builtin_display_disabled != backlight->builtin_display_disabled) {
-                backlight->builtin_display_disabled = builtin_display_disabled;
-                g_object_notify_by_pspec (G_OBJECT (backlight), props[PROP_BRIGHTNESS]);
-        }
+        return backlight->backlight_connector;
 }
 
 static void
@@ -773,10 +741,6 @@ gsd_backlight_get_property (GObject    *object,
         GsdBacklight *backlight = GSD_BACKLIGHT (object);
 
         switch (prop_id) {
-        case PROP_RR_SCREEN:
-                g_value_set_object (value, backlight->rr_screen);
-                break;
-
         case PROP_BRIGHTNESS:
                 g_value_set_int (value, gsd_backlight_get_brightness (backlight, NULL));
                 break;
@@ -788,28 +752,59 @@ gsd_backlight_get_property (GObject    *object,
 }
 
 static void
-gsd_backlight_set_property (GObject      *object,
-                            guint         prop_id,
-                            const GValue *value,
-                            GParamSpec   *pspec)
+update_mutter_backlight (GsdBacklight *backlight)
 {
-        GsdBacklight *backlight = GSD_BACKLIGHT (object);
+        GsdDisplayConfig *display_config =
+                gnome_settings_bus_get_display_config_proxy ();
+        GVariant *backlights = NULL;
+        g_autoptr(GVariant) monitors = NULL;
+        g_autoptr(GVariant) monitor = NULL;
+        gboolean monitor_active = FALSE;
+        gboolean have_backlight = FALSE;
 
-        switch (prop_id) {
-        case PROP_RR_SCREEN:
-                backlight->rr_screen = g_value_dup_object (value);
-
-                g_signal_connect_object (backlight->rr_screen, "changed",
-                                         G_CALLBACK (gsd_backlight_rr_screen_changed_cb),
-                                         object, 0);
-                gsd_backlight_rr_screen_changed_cb (backlight->rr_screen, object);
-
-                break;
-
-        default:
-                G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-                break;
+        backlights = gsd_display_config_get_backlight (display_config);
+        g_variant_get (backlights, "(u@aa{sv})",
+                       &backlight->backlight_serial,
+                       &monitors);
+        if (g_variant_n_children (monitors) > 1) {
+                g_warning ("Only handling the first out of %lu backlight monitors",
+                           g_variant_n_children (monitors));
         }
+
+        if (g_variant_n_children (monitors) > 0) {
+                g_variant_get_child (monitors, 0, "a{sv}", &monitor);
+
+                g_clear_pointer (&backlight->backlight_connector, g_free);
+                g_variant_lookup (monitor, "connector", "s",
+                                  &backlight->backlight_connector);
+                g_variant_lookup (monitor, "active", "b", &monitor_active);
+                backlight->builtin_display_disabled = !monitor_active;
+
+                if (g_variant_lookup (monitor, "value", "i", &backlight->brightness_val)) {
+                        g_variant_lookup (monitor, "min", "i", &backlight->brightness_min);
+                        g_variant_lookup (monitor, "max", "i", &backlight->brightness_max);
+                        have_backlight = TRUE;
+                }
+        }
+
+        if (!have_backlight) {
+                backlight->brightness_val = -1;
+                backlight->brightness_min = -1;
+                backlight->brightness_max = -1;
+        }
+}
+
+static void
+on_backlight_changed (GsdDisplayConfig *display_config,
+                      GsdBacklight     *backlight)
+{
+        gboolean builtin_display_disabled;
+
+        builtin_display_disabled = backlight->builtin_display_disabled;
+        update_mutter_backlight (backlight);
+
+        if (builtin_display_disabled != backlight->builtin_display_disabled)
+                g_object_notify_by_pspec (G_OBJECT (backlight), props[PROP_BRIGHTNESS]);
 }
 
 static gboolean
@@ -818,7 +813,8 @@ gsd_backlight_initable_init (GInitable       *initable,
                              GError         **error)
 {
         GsdBacklight *backlight = GSD_BACKLIGHT (initable);
-        GnomeRROutput* output = NULL;
+        GsdDisplayConfig *display_config =
+                gnome_settings_bus_get_display_config_proxy ();
         GError *logind_error = NULL;
 
         if (cancellable != NULL) {
@@ -826,6 +822,18 @@ gsd_backlight_initable_init (GInitable       *initable,
                                      "GsdBacklight does not support cancelling initialization.");
                 return FALSE;
         }
+
+        if (!display_config) {
+                g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
+                                     "GsdBacklight needs org.gnome.Mutter.DisplayConfig to function");
+                return FALSE;
+        }
+
+        g_signal_connect (display_config,
+                          "notify::backlight",
+                          G_CALLBACK (on_backlight_changed),
+                          backlight);
+        update_mutter_backlight (backlight);
 
 #ifdef __linux__
         backlight->logind_proxy =
@@ -871,15 +879,8 @@ gsd_backlight_initable_init (GInitable       *initable,
                 goto found;
 #endif /* __linux__ */
 
-        /* Try GNOME RR as a fallback. */
-        output = gsd_backlight_rr_find_output (backlight, TRUE);
-        if (output) {
-                g_debug ("Using GNOME RR (mutter) for backlight.");
-                backlight->brightness_min = 1;
-                backlight->brightness_max = 100;
-                backlight->brightness_val = gnome_rr_output_get_backlight (output);
-                backlight->brightness_step = gnome_rr_output_get_min_backlight_step (output);
-
+        if (backlight->backlight_connector) {
+                g_debug ("Using DisplayConfig (mutter) for backlight.");
                 goto found;
         }
 
@@ -908,6 +909,7 @@ gsd_backlight_finalize (GObject *object)
         g_assert (backlight->active_task == NULL);
         g_assert (g_queue_is_empty (&backlight->tasks));
         g_clear_object (&backlight->logind_proxy);
+        g_clear_pointer (&backlight->backlight_connector, g_free);
         g_clear_object (&backlight->udev);
         g_clear_object (&backlight->udev_device);
         if (backlight->idle_update) {
@@ -915,8 +917,6 @@ gsd_backlight_finalize (GObject *object)
                 backlight->idle_update = 0;
         }
 #endif /* __linux__ */
-
-        g_clear_object (&backlight->rr_screen);
 }
 
 static void
@@ -932,12 +932,6 @@ gsd_backlight_class_init (GsdBacklightClass *klass)
 
         object_class->finalize = gsd_backlight_finalize;
         object_class->get_property = gsd_backlight_get_property;
-        object_class->set_property = gsd_backlight_set_property;
-
-        props[PROP_RR_SCREEN] = g_param_spec_object ("rr-screen", "GnomeRRScreen",
-                                                     "GnomeRRScreen usable for backlight control.",
-                                                     GNOME_TYPE_RR_SCREEN,
-                                                     G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
 
         props[PROP_BRIGHTNESS] = g_param_spec_int ("brightness", "The display brightness",
                                                    "The brightness of the internal display in percent.",
@@ -964,11 +958,9 @@ gsd_backlight_init (GsdBacklight *backlight)
 }
 
 GsdBacklight *
-gsd_backlight_new (GnomeRRScreen  *rr_screen,
-                   GError        **error)
+gsd_backlight_new (GError **error)
 {
         return GSD_BACKLIGHT (g_initable_new (GSD_TYPE_BACKLIGHT, NULL, error,
-                                              "rr-screen", rr_screen,
                                               NULL));
 }
 

plugins/power/gpm-common.c
@@ -31,9 +31,6 @@
 #include <X11/extensions/dpms.h>
 #include <canberra-gtk.h>
 
-#define GNOME_DESKTOP_USE_UNSTABLE_API
-#include <libgnome-desktop/gnome-rr.h>
-
 #include "gnome-settings-bus.h"
 #include "gpm-common.h"
 #include "gsd-power-constants.h"
@@ -317,39 +314,21 @@ get_mock_external_monitor_file (void)
 	  return mocked_once.retval;
 }
 
-static gboolean
-randr_output_is_on (GnomeRROutput *output)
-{
-        GnomeRRCrtc *crtc;
-
-        crtc = gnome_rr_output_get_crtc (output);
-        if (!crtc)
-                return FALSE;
-        return gnome_rr_crtc_get_current_mode (crtc) != NULL;
-}
-
 static void
 mock_monitor_changed (GFileMonitor     *monitor,
 		      GFile            *file,
 		      GFile            *other_file,
-		      GFileMonitorEvent event_type,
-		      gpointer          user_data)
+		      GFileMonitorEvent event_type)
 {
-	GnomeRRScreen *screen = (GnomeRRScreen *) user_data;
+        GsdDisplayConfig *display_config =
+                gnome_settings_bus_get_display_config_proxy ();
 
-	g_debug ("Mock screen configuration changed");
-	g_signal_emit_by_name (G_OBJECT (screen), "changed");
-}
-
-static void
-screen_destroyed (gpointer  user_data,
-		  GObject  *where_the_object_was)
-{
-	g_object_unref (G_OBJECT (user_data));
+	g_debug ("Emitting mocked has-external-monitor property changed signal on %p", display_config);
+	g_object_notify (G_OBJECT (display_config), "has-external-monitor");
 }
 
 void
-watch_external_monitor (GnomeRRScreen *screen)
+watch_external_monitor (void)
 {
 	const gchar *filename;
 	GFile *file;
@@ -363,12 +342,11 @@ watch_external_monitor (GnomeRRScreen *screen)
 	monitor = g_file_monitor (file, G_FILE_MONITOR_NONE, NULL, NULL);
 	g_object_unref (file);
 	g_signal_connect (monitor, "changed",
-			  G_CALLBACK (mock_monitor_changed), screen);
-	g_object_weak_ref (G_OBJECT (screen), screen_destroyed, monitor);
+			  G_CALLBACK (mock_monitor_changed), NULL);
 }
 
 static gboolean
-mock_external_monitor_is_connected (GnomeRRScreen *screen)
+mock_external_monitor_is_connected (void)
 {
 	char *mock_external_monitor_contents;
 	const gchar *filename;
@@ -395,25 +373,42 @@ mock_external_monitor_is_connected (GnomeRRScreen *screen)
 }
 
 gboolean
-external_monitor_is_connected (GnomeRRScreen *screen)
+external_monitor_is_connected (void)
 {
-        GnomeRROutput **outputs;
-        guint i;
+        GsdDisplayConfig *display_config =
+                gnome_settings_bus_get_display_config_proxy ();
+        GDBusConnection *connection;
+        g_autoptr (GError) error = NULL;
+        g_autoptr (GVariant) variant = NULL;
+        g_autoptr (GVariant) inner = NULL;
 
         if (get_mock_external_monitor_file ())
-                return mock_external_monitor_is_connected (screen);
+                return mock_external_monitor_is_connected ();
 
-	g_assert (screen != NULL);
+        /* This needs to be a synchronous call, an up to date state is needed
+         * in response to lid changes. */
 
-        /* see if we have more than one screen plugged in */
-        outputs = gnome_rr_screen_list_outputs (screen);
-        for (i = 0; outputs[i] != NULL; i++) {
-                if (randr_output_is_on (outputs[i]) &&
-                    !gnome_rr_output_is_builtin_display (outputs[i]))
-                        return TRUE;
+        connection = g_dbus_proxy_get_connection (G_DBUS_PROXY (display_config));
+        variant = g_dbus_connection_call_sync (connection,
+                                               "org.gnome.Mutter.DisplayConfig",
+                                               "/org/gnome/Mutter/DisplayConfig",
+                                               "org.freedesktop.DBus.Properties",
+                                               "Get",
+                                               g_variant_new ("(ss)",
+                                                              "org.gnome.Mutter.DisplayConfig",
+                                                              "HasExternalMonitor"),
+                                               NULL,
+                                               G_DBUS_CALL_FLAGS_NONE,
+                                               -1,
+                                               NULL,
+                                               &error);
+        if (!variant) {
+                g_debug ("Failed to get property 'HasExternalMonitor': %s", error->message);
+                return FALSE;
         }
 
-        return FALSE;
+        g_variant_get (variant, "(v)", &inner);
+        return g_variant_get_boolean (inner);
 }
 
 static void

plugins/power/gpm-common.h
@@ -47,8 +47,8 @@ int              gsd_power_backlight_abs_to_percentage  (int min, int max, int v
 int              gsd_power_backlight_percentage_to_abs  (int min, int max, int value);
 
 /* RandR helpers */
-void             watch_external_monitor                 (GnomeRRScreen *screen);
-gboolean         external_monitor_is_connected          (GnomeRRScreen *screen);
+void             watch_external_monitor                 (void);
+gboolean         external_monitor_is_connected          (void);
 
 /* Sound helpers */
 void             play_loop_start                        (guint *id);

plugins/power/gsd-backlight.h
@@ -24,10 +24,6 @@
 #include <glib.h>
 #include <gio/gio.h>
 
-#define GNOME_DESKTOP_USE_UNSTABLE_API
-#include <libgnome-desktop/gnome-rr.h>
-
-
 G_BEGIN_DECLS
 
 #define GSD_TYPE_BACKLIGHT gsd_backlight_get_type ()
@@ -72,8 +68,7 @@ gint gsd_backlight_cycle_up_finish       (GsdBacklight         *backlight,
 
 const char*  gsd_backlight_get_connector (GsdBacklight         *backlight);
 
-GsdBacklight* gsd_backlight_new          (GnomeRRScreen        *screen,
-                                          GError              **error);
+GsdBacklight* gsd_backlight_new          (GError              **error);
 
 
 G_END_DECLS

commit 84cbbf02d06ffb5568137f6f47761c7835d556d9

power: Fix GVariant format string for getting monitor
The `monitor` is expected to be a GVariant, so we need to include the
`@` here.

Fixes: 7f8e33c7 ("power: Stop using libgnome-rr")
Closes: https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/issues/817

plugins/power/gsd-backlight.c
@@ -772,7 +772,7 @@ update_mutter_backlight (GsdBacklight *backlight)
         }
 
         if (g_variant_n_children (monitors) > 0) {
-                g_variant_get_child (monitors, 0, "a{sv}", &monitor);
+                g_variant_get_child (monitors, 0, "@a{sv}", &monitor);
 
                 g_clear_pointer (&backlight->backlight_connector, g_free);
                 g_variant_lookup (monitor, "connector", "s",