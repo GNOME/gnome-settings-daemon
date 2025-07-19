12bb83f544de4d4258f67010a447c6e612f8d43a
Adrian Vovk <adrianvovk@gmail.com>
2025-07-18 23:42:57 -0400

    a11y-settings: Manage Orca's lifecycle
    Previously, Orca was launched by gnome-session via a .desktop file. The
    desktop file instructed gnome-session to monitor the
    screen-reader-enabled gsetting and run Orca for as long as it's true.
    
    Since GNOME 49, gnome-session doesn't use .desktop files to launch
    session services anymore, and has removed support for autostart
    conditions. Thus, gnome-session no longer watches the
    screen-reader-enabled gsetting.
    
    Luckily, gsd-a11y-settings monitors the gsetting, so this is a natural
    place for this logic.
    
    Note that we call this codepath the "fallback" because a future commit
    will introduce a codepath that uses systemd to launch Orca, and the code
    introduced here will be used only when systemd fails

--- plugins/a11y-settings/gsd-a11y-settings-manager.c
+++ plugins/a11y-settings/gsd-a11y-settings-manager.c
@@ -31,6 +31,8 @@
 
 #include <glib.h>
 #include <gio/gio.h>
+#define GNOME_DESKTOP_USE_UNSTABLE_API
+#include <libgnome-desktop/gnome-systemd.h>
 
 #include "gnome-settings-profile.h"
 #include "gsd-a11y-settings-manager.h"
@@ -41,6 +43,8 @@ struct _GsdA11ySettingsManager
 
         GSettings *interface_settings;
         GSettings *a11y_apps_settings;
+
+        GPid       fallback_orca;
 };
 
 enum {
@@ -51,6 +55,56 @@ static void     gsd_a11y_settings_manager_class_init  (GsdA11ySettingsManagerCla
 static void     gsd_a11y_settings_manager_init        (GsdA11ySettingsManager      *a11y_settings_manager);
 
 G_DEFINE_TYPE (GsdA11ySettingsManager, gsd_a11y_settings_manager, GSD_TYPE_APPLICATION)
+
+static void
+kill_fallback_orca (GsdA11ySettingsManager *manager)
+{
+        if (manager->fallback_orca == 0)
+                return;
+
+        if (kill(manager->fallback_orca, SIGTERM) < 0)
+                g_warning ("Failed to kill fallback Orca (pid=%d): %m", manager->fallback_orca);
+        else
+                g_debug ("Killed fallback Orca (pid=%d)", manager->fallback_orca);
+
+        manager->fallback_orca = 0;
+}
+
+static void
+spawn_fallback_orca (GsdA11ySettingsManager *manager)
+{
+        char *argv[] = { "orca", "--replace", NULL };
+        GDBusConnection *connection = g_application_get_dbus_connection (G_APPLICATION (manager));
+        g_autoptr (GError) error = NULL;
+        const gchar *workdir;
+        GPid pid;
+        gboolean success;
+
+        kill_fallback_orca (manager);
+
+        workdir = g_get_home_dir ();
+        success = g_spawn_async (workdir, argv, NULL, G_SPAWN_SEARCH_PATH, NULL,
+                                 NULL, &pid, &error);
+        if (!success) {
+                g_warning ("Failed to spawn fallback Orca: %s", error->message);
+                return;
+        }
+
+        gnome_start_systemd_scope ("orca", pid, NULL, connection, NULL, NULL, NULL);
+
+        g_debug ("Spawned fallback Orca (pid=%d)", pid);
+        manager->fallback_orca = pid;
+}
+
+static void
+manage_orca (GsdA11ySettingsManager *manager,
+             gboolean                enabled)
+{
+        if (enabled)
+                spawn_fallback_orca (manager);
+        else
+                kill_fallback_orca (manager);
+}
 
 static void
 apps_settings_changed (GSettings              *settings,
@@ -80,12 +134,16 @@ apps_settings_changed (GSettings              *settings,
                 g_debug ("Disabling toolkit-accessibility, screen reader, OSK and magnifier disabled");
                 g_settings_set_boolean (manager->interface_settings, "toolkit-accessibility", FALSE);
         }
+
+        if (screen_reader_changed)
+                manage_orca (manager, screen_reader_enabled);
 }
 
 static void
 gsd_a11y_settings_manager_startup (GApplication *app)
 {
         GsdA11ySettingsManager *manager = GSD_A11Y_SETTINGS_MANAGER (app);
+        gboolean screen_reader_enabled;
 
         g_debug ("Starting a11y_settings manager");
         gnome_settings_profile_start (NULL);
@@ -100,9 +158,11 @@ gsd_a11y_settings_manager_startup (GApplication *app)
          * enabled, make sure a11y is enabled for the toolkits.
          * We don't do the same thing for the reverse so it's possible to
          * enable AT-SPI for the toolkits without using an a11y app */
+        screen_reader_enabled = g_settings_get_boolean (manager->a11y_apps_settings, "screen-reader-enabled");
+        manage_orca (manager, screen_reader_enabled);
         if (g_settings_get_boolean (manager->a11y_apps_settings, "screen-keyboard-enabled") ||
-            g_settings_get_boolean (manager->a11y_apps_settings, "screen-reader-enabled") ||
-            g_settings_get_boolean (manager->a11y_apps_settings, "screen-magnifier-enabled"))
+            g_settings_get_boolean (manager->a11y_apps_settings, "screen-magnifier-enabled") ||
+            screen_reader_enabled)
                 g_settings_set_boolean (manager->interface_settings, "toolkit-accessibility", TRUE);
 
         G_APPLICATION_CLASS (gsd_a11y_settings_manager_parent_class)->startup (app);
@@ -114,6 +174,8 @@ static void
 gsd_a11y_settings_manager_shutdown (GApplication *app)
 {
         GsdA11ySettingsManager *manager = GSD_A11Y_SETTINGS_MANAGER (app);
+
+        kill_fallback_orca (manager);
 
         g_clear_object (&manager->interface_settings);
         g_clear_object (&manager->a11y_apps_settings);
