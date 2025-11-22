GNOME/gnome-settings-daemon

commit 54b5d05e89873158b20fe83a1f7f7bd44bc10e72

housekeeping: Downgrade donation reminder urgency to "normal"
Critical notifications can bypass the do-not-disturbe settings and
interrupt fullscreen video/presentations.

We don't want our donation campaigns to disturb users in focus moments.

Reported in https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/issues/896#note_2575412

plugins/housekeeping/gsd-donation-reminder.c
@@ -80,7 +80,7 @@ show_notification (void)
 
         notify_notification_set_app_name (notification, _("GNOME"));
         notify_notification_set_hint (notification, "transient", g_variant_new_boolean (TRUE));
-        notify_notification_set_urgency (notification, NOTIFY_URGENCY_CRITICAL);
+        notify_notification_set_urgency (notification, NOTIFY_URGENCY_NORMAL);
         notify_notification_set_timeout (notification, NOTIFY_EXPIRES_DEFAULT);
         notify_notification_set_hint_string (notification, "desktop-entry", "gnome-about-panel");
 

commit b39840dee58c28bb4558d2adaf2b84a05bbde3d2

housekeeping: Don't mark donation reminder notification as transient
When the notification was still marked as critical prior to 54b5d05e,
setting the transient hint would ensure that it was not showing up in
the calendar menu, which made sense because it was shown on screen until
manually closed or interacted with. However now that it is not critical
anymore, this means the notification disappears after being hovered and
unhovered, without any other user interaction.

Fixes: 54b5d05e ("housekeeping: Downgrade donation reminder urgency to "normal"")

plugins/housekeeping/gsd-donation-reminder.c
@@ -79,7 +79,6 @@ show_notification (void)
                           NULL);
 
         notify_notification_set_app_name (notification, _("GNOME"));
-        notify_notification_set_hint (notification, "transient", g_variant_new_boolean (TRUE));
         notify_notification_set_urgency (notification, NOTIFY_URGENCY_NORMAL);
         notify_notification_set_timeout (notification, NOTIFY_EXPIRES_DEFAULT);
         notify_notification_set_hint_string (notification, "desktop-entry", "gnome-about-panel");