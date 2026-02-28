71e5fb7b18543722884e6ff684f5ef6e1ae61bb6
Adrian Vovk <adrianvovk@gmail.com>
2026-02-27 23:27:49 -0500

    media-keys: Ignore suspend inhibitors
    At the moment, if suspend is inhibited at the moment that the user
    presses the suspend or hibernate button, nothing will happen. Without
    this flag, systemd won't even prompt with Polkit to allow the user to
    ignore the inhibitors should they choose to do so.
    
    Note that ignoring a suspend inhibitor isn't very bad, because systemd
    will ignore all suspend inhibitors implicitly whenever a laptop's lid is
    closed. This simply extends this treatment to the UI, with the
    additional safety of the Polkit prompt.

--- plugins/media-keys/gsd-media-keys-manager.c
+++ plugins/media-keys/gsd-media-keys-manager.c
@@ -107,6 +107,7 @@
 #define SYSTEMD_DBUS_NAME                       "org.freedesktop.login1"
 #define SYSTEMD_DBUS_PATH                       "/org/freedesktop/login1"
 #define SYSTEMD_DBUS_INTERFACE                  "org.freedesktop.login1.Manager"
+#define SD_LOGIND_SKIP_INHIBITORS               (UINT64_C(1) << 4)
 
 #define AUDIO_SELECTION_DBUS_NAME               "org.gnome.Shell.AudioDeviceSelection"
 #define AUDIO_SELECTION_DBUS_PATH               "/org/gnome/Shell/AudioDeviceSelection"
@@ -1998,16 +1999,41 @@ do_toggle_contrast_action (GsdMediaKeysManager *manager)
 }
 
 static void
-power_action (GsdMediaKeysManager *manager,
-              const char          *action,
-              gboolean             allow_interaction)
+logind_power_action (GsdMediaKeysManager *manager,
+                     GsdPowerActionType   action,
+                     gboolean             allow_interaction)
 {
         GsdMediaKeysManagerPrivate *priv = GSD_MEDIA_KEYS_MANAGER_GET_PRIVATE (manager);
+        const char *method;
+        guint64 logind_flags = 0;
+        GDBusCallFlags dbus_flags = G_DBUS_CALL_FLAGS_NONE;
+
+        switch (action) {
+        case GSD_POWER_ACTION_SUSPEND:
+                method = "SuspendWithFlags";
+                logind_flags = SD_LOGIND_SKIP_INHIBITORS;
+                break;
+        case GSD_POWER_ACTION_SHUTDOWN:
+                method = "PowerOffWithFlags";
+                break;
+        case GSD_POWER_ACTION_HIBERNATE:
+                method = "HibernateWithFlags";
+                logind_flags = SD_LOGIND_SKIP_INHIBITORS;
+                break;
+        case GSD_POWER_ACTION_INTERACTIVE:
+        case GSD_POWER_ACTION_BLANK:
+        case GSD_POWER_ACTION_LOGOUT:
+        case GSD_POWER_ACTION_NOTHING:
+                g_assert_not_reached ();
+        }
+
+        if (allow_interaction)
+                dbus_flags = G_DBUS_CALL_FLAGS_ALLOW_INTERACTIVE_AUTHORIZATION;
 
         g_dbus_proxy_call (priv->logind_proxy,
-                           action,
-                           g_variant_new ("(b)", allow_interaction),
-                           G_DBUS_CALL_FLAGS_NONE,
+                           method,
+                           g_variant_new ("(t)", logind_flags),
+                           dbus_flags,
                            G_MAXINT,
                            priv->bus_cancellable,
                            NULL, NULL);
@@ -2020,17 +2046,13 @@ do_config_power_action (GsdMediaKeysManager *manager,
 {
         switch (action_type) {
         case GSD_POWER_ACTION_SUSPEND:
-                power_action (manager, "Suspend", !in_lock_screen);
+        case GSD_POWER_ACTION_SHUTDOWN:
+        case GSD_POWER_ACTION_HIBERNATE:
+                logind_power_action (manager, action_type, !in_lock_screen);
                 break;
         case GSD_POWER_ACTION_INTERACTIVE:
                 if (!in_lock_screen)
                         gnome_session_shutdown (manager);
-                break;
-        case GSD_POWER_ACTION_SHUTDOWN:
-                power_action (manager, "PowerOff", !in_lock_screen);
-                break;
-        case GSD_POWER_ACTION_HIBERNATE:
-                power_action (manager, "Hibernate", !in_lock_screen);
                 break;
         case GSD_POWER_ACTION_BLANK:
         case GSD_POWER_ACTION_LOGOUT:
@@ -2123,7 +2145,7 @@ do_config_power_button_action (GsdMediaKeysManager *manager,
                 g_warning_once ("Virtual machines only honor the 'nothing' power-button-action, and will shutdown otherwise");
 
                 if (action_type != GSD_POWER_BUTTON_ACTION_NOTHING)
-                        power_action (manager, "PowerOff", FALSE);
+                        logind_power_action (manager, GSD_POWER_ACTION_SHUTDOWN, FALSE);
 
                 return;
         }
